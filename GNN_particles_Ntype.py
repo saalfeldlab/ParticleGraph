c21b3cb03f4bfe4d5f3937604042c704397d4186
allierc <allierc@allierc-ls1.hhmi.org>
2023-09-10 09:27:15 -0400

    try

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -421,7 +421,7 @@ if __name__ == '__main__':
 
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     print(device)
 
     # model_config = {'ntry': 612,
