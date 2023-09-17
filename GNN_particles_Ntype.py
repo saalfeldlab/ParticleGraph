24b5f2f12c83c6a8659f26455c57af3355abc260
allierc <allierc@allierc-ls1.hhmi.org>
2023-09-16 22:11:01 -0400

    na

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -885,7 +885,7 @@ def data_test(model_config,index_particles, prev_nparticles, new_nparticles, pre
     NGraphs = int(len(graph_files) / nframes)
     print('Graph files N: ', NGraphs-1)
 
-    net = f"./log/try_{ntry}/models/best_model_with_{NGraphs-1}_graphs.pt"
+    net = f"./log/try_{ntry}/models/best_model_with_2_graphs.pt"
     print(f'network: {net}')
     state_dict = torch.load(net)
     model.load_state_dict(state_dict['model_state_dict'])
@@ -1241,7 +1241,7 @@ if __name__ == '__main__':
     print('')
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(f'device {device}')
 
     model_config = {'ntry': 602,
@@ -1344,14 +1344,15 @@ if __name__ == '__main__':
                     'data_augmentation' : True,
                     'model': 'InteractionParticles'}
 
+
     gtest_list=[1,2,5,10]
 
-    for gtest in range(12):
+    for gtest in range(1):
 
-            ntry=585+gtest
-            model_config['noise_level'] =  gtest_list[gtest%4] / 100
-            model_config['noise_type'] = 1 + gtest // 4
-            model_config['ntry'] = ntry
+            # ntry=585+gtest
+            # model_config['noise_level'] =  gtest_list[gtest%4] / 100
+            # model_config['noise_type'] = 1 + gtest // 4
+            # model_config['ntry'] = ntry
             # model_config['hidden_size'] = gtest_list[gtest]
             datum = model_config['datum']
             # datum = '230902_' + str(ntry)
@@ -1383,12 +1384,11 @@ if __name__ == '__main__':
 
             time.sleep(0.5)
 
-            if gtest==0:
-                data_generate(model_config,index_particles)
-            data_train(model_config,index_particles)
+            # data_generate(model_config,index_particles)
+            # data_train(model_config,index_particles)
             data_test(model_config, index_particles, prev_nparticles=0, new_nparticles=0, prev_index_particles=0)
-            # prev_nparticles, new_nparticles, prev_index_particles = data_test_generate(model_config,index_particles)
-            # data_test(model_config,index_particles,prev_nparticles, new_nparticles, prev_index_particles)
+            #prev_nparticles, new_nparticles, prev_index_particles = data_test_generate(model_config,index_particles)
+            #data_test(model_config,index_particles,prev_nparticles, new_nparticles, prev_index_particles)
 
 
 
