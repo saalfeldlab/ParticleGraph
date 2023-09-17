04f3b07f8b5f3b0c7f3cc7f3d0d739012c826392
allierc <allierc@allierc-ls1.hhmi.org>
2023-09-16 22:30:56 -0400

    na

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -1115,7 +1115,7 @@ def data_test_generate(model_config,index_particles):
     prev_nparticles = nparticles
     prev_index_particles = index_particles
 
-    new_nparticles = 4000
+    new_nparticles = 2000
     nparticles = new_nparticles
 
     index_particles = []
@@ -1126,9 +1126,9 @@ def data_test_generate(model_config,index_particles):
     X1 = torch.rand(nparticles, 2, device=device)
 
     # scenario A
-    X1[:, 0] = X1[:, 0] / nparticle_types
-    for n in range(nparticle_types):
-        X1[index_particles[n], 0] = X1[index_particles[n], 0] + n / nparticle_types
+    # X1[:, 0] = X1[:, 0] / nparticle_types
+    # for n in range(nparticle_types):
+    #     X1[index_particles[n], 0] = X1[index_particles[n], 0] + n / nparticle_types
 
     # scenario B
     # X1[index_particles[0], :] = X1[index_particles[0], :]/2 + 1/4
@@ -1388,7 +1388,7 @@ if __name__ == '__main__':
             # data_train(model_config,index_particles)
             # cdata_test(model_config, index_particles, prev_nparticles=0, new_nparticles=0, prev_index_particles=0)
             prev_nparticles, new_nparticles, prev_index_particles = data_test_generate(model_config,index_particles)
-            data_test(model_config,index_particles,prev_nparticles, new_nparticles, prev_index_particles)
+            data_test(model_config, index_particles, prev_nparticles, new_nparticles, prev_index_particles)
 
 
 
