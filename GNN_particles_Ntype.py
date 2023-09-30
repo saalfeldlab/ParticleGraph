d4e9083910236250f8e8bbb80341520f35b4b6f5
allierc <allierc@allierc.janelia.hhmi.org>
2023-09-30 16:25:08 -0400

    na

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -3113,7 +3113,7 @@ if __name__ == '__main__':
         for key, value in model_config.items():
             print(key, ":", value)
         data_generate(model_config)
-        # data_train(model_config,gtest)
+        data_train(model_config,gtest)
         # x, rmserr_list = data_test(model_config, bVisu=True, bPrint=True)
         # prev_nparticles, new_nparticles, prev_index_particles, index_particles = data_test_generate(model_config)
         # x, rmserr_list = data_test(model_config, bVisu = True, bPrint=True, index_particles=index_particles, prev_nparticles=prev_nparticles, new_nparticles=new_nparticles, prev_index_particles=prev_index_particles)
