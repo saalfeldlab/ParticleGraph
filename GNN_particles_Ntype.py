f96cf7ccfe171edd0b007b5be75b5c7fa146a38d
cedric <allierc@janelia.hhmi.org>
2023-11-13 09:06:57 -0500

    version 1.6 fixing second derivative

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -4249,7 +4249,7 @@ def load_model_config(id=48):
 if __name__ == '__main__':
 
     print('')
-    print('version 1.5 231024')
+    print('version 1.6 231113')
     print('use of https://github.com/gpeyre/.../ml_10_particle_system.ipynb')
     print('')
 
@@ -4260,7 +4260,7 @@ if __name__ == '__main__':
     S_e = SamplesLoss(loss="sinkhorn", p=2, blur=.05)
 
 
-    gtestlist = [88] #[123, 140, 141, 73, 123] # [75,84,85]
+    gtestlist = [138,139,140,144] #[123, 140, 141, 73, 123] # [75,84,85]
 
     for gtest in gtestlist:
 
@@ -4289,13 +4289,13 @@ if __name__ == '__main__':
             def bc_diff(D):
                 return torch.remainder(D - .5, 1.0) - .5
 
-        if gtest>=140:
-            data_generate_boid(model_config, bVisu=True, bDetails=True, bSave=True, step=1)
-        else:
-            data_generate(model_config, bVisu=True, bDetails=True, bSave=True, step=5)
+        # if gtest>=140:
+        #     data_generate_boid(model_config, bVisu=True, bDetails=True, bSave=True, step=1)
+        # else:
+        #     data_generate(model_config, bVisu=True, bDetails=True, bSave=True, step=5)
         # data_train(model_config, bSparse=False)
         # data_plot(model_config, epoch=-1, bPrint=True, best_model=20)
-        # x, rmserr_list = data_test(model_config, bVisu=True, bPrint=True, best_model=-1, step=100)
+        x, rmserr_list = data_test(model_config, bVisu=True, bPrint=True, best_model=-1, step=10)
         # prev_nparticles, new_nparticles, prev_index_particles, index_particles = data_test_generate(model_config, bVisu=True, bDetails=True, step=10)
         # x, rmserr_list = data_test(model_config, bVisu = True, bPrint=True, index_particles=index_particles, prev_nparticles=prev_nparticles, new_nparticles=new_nparticles, prev_index_particles=prev_index_particles, best_model=-1, step=100)
 
