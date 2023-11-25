c3750a9286cf0dc7a2a96eb8e26ef87cbd69b94a
allierc <allierc@allierc.janelia.hhmi.org>
2023-11-25 18:58:34 -0500

    na

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -3866,13 +3866,13 @@ if __name__ == '__main__':
     print('use of https://github.com/gpeyre/.../ml_10_particle_system.ipynb')
     print('')
 
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
     print(f'device {device}')
 
     scaler = StandardScaler()
     S_e = SamplesLoss(loss="sinkhorn", p=2, blur=.05)
 
-    gtestlist = [127] #[123, 140, 141, 73, 123] # [75,84,85]
+    gtestlist = [128] #[123, 140, 141, 73, 123] # [75,84,85]
 
     for gtest in gtestlist:
 
@@ -3907,7 +3907,7 @@ if __name__ == '__main__':
             data_generate(model_config, bVisu=True, bDetails=True, bErase=False, step=5)
         data_train(model_config)
         x, rmserr_list = data_test(model_config, bVisu=True, bPrint=True, best_model=13, step=5, bTest='', initial_map='')
-        
+
         # data_plot(model_config, epoch=-1, bPrint=True, best_model=-1)
         # prev_nparticles, new_nparticles, prev_index_particles, index_particles = data_test_generate(model_config, bVisu=True, bDetails=True, step=10)
         # x, rmserr_list = data_test(model_config, bVisu = True, bPrint=True, index_particles=index_particles, prev_nparticles=prev_nparticles, new_nparticles=new_nparticles, prev_index_particles=prev_index_particles, best_model=-1, step=100)
