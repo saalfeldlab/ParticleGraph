5e101be8437dd4cc245478d77a019a449141a065
Michael Innerberger <innerbergerm@hhmi.org>
2023-12-19 16:56:13 -0500

    Add more data cleaning and output

--- data_loaders.py
+++ data_loaders.py
@@ -7,23 +7,36 @@ import torch
 import pandas as pd
 
 
-def load_shrofflab_celegans(file_path):
+def load_shrofflab_celegans(
+        file_path,
+        *,
+        replace_missing_cpm=None
+        ):
     """
     Load the Shrofflab C. elegans data from a CSV file and convert it to a PyTorch tensor.
 
     Args:
         file_path (str): The path to the CSV file.
+        replace_missing_cpm (float): If not None, replace missing cpm values with this value.
 
     Returns:
         torch_tensor (torch.Tensor): A PyTorch tensor containing the loaded data.
         times (np.ndarray): The time points corresponding to the data.
     """
 
-    # Load the data from the CSV file and drop rows with missing time values
+    # Load the data from the CSV file and clean it a bit:
+    # - drop rows with missing time values (occurs only at the end of the data)
+    # - fill missing cpm values (don't interpolate because data is missing at the beginning or end)
+    print(f"Loading data from {file_path}...")
     dtypes = {"time": np.float32, "x": np.float32, "y": np.float32,
               "z": np.float32, "cell": str, "log10 mean cpm": str}
     data = pd.read_csv(file_path, dtype=dtypes)
+    print(f"Loaded {data.shape[0]} rows of data, dropping rows with missing time values...")
     data.dropna(subset=["time"], inplace=True)
+    print(f"Remaining: {data.shape[0]} rows")
+    if replace_missing_cpm is not None:
+        print(f"Filling missing cpm values with {replace_missing_cpm}...")
+        data.fillna(replace_missing_cpm, inplace=True)
 
     # Find the indices where the data for each cell begins (time resets)
     time_reset = np.where(np.diff(data["time"]) < 0)[0] + 1
@@ -43,7 +56,7 @@ def load_shrofflab_celegans(file_path):
         first_cell_data = timeseries_boundaries[:-1]
         abnormal_data = first_cell_data[n_timepoints != n_normal_timepoints]
         abnormal_cells = data["cell"][abnormal_data].values
-        print(f"Warning: incomplete time series data will be replaced by NaN: {abnormal_cells}")
+        print(f"Warning: incomplete time series data will be replaced by NaN for {abnormal_cells}")
 
     # Put values into a 3D tensor
     relevant_fields = ["x", "y", "z", "log10 mean cpm"]
