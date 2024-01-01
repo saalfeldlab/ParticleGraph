ddc0217be03802f7e834d836d5662683440f012b
allierc <allierc@allierc.janelia.hhmi.org>
2024-01-01 11:17:44 -0500

    Nepochs in config train wave Coulomb_3 gravity_16 minradius0.001

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -3462,8 +3462,8 @@ if __name__ == '__main__':
             def bc_diff(D):
                 return torch.remainder(D - .5, 1.0) - .5
 
-        # data_generate(model_config, bVisu=False, bDetails=False, alpha=0.2, bErase=False, bLoad_p=False, step=400)
-        # data_train(model_config,model_embedding)
+        data_generate(model_config, bVisu=False, bDetails=False, alpha=0.2, bErase=False, bLoad_p=False, step=400)
+        data_train(model_config,model_embedding)
         data_plot(model_config, epoch=-1, bPrint=True, best_model=20)
         # data_test(model_config, bVisu=True, bPrint=True, best_model=20, bDetails=False, step=10) # model_config['nframes']-5)
 
