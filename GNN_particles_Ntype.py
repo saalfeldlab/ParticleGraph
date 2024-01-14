17d935fda2a5ca3ce73881a6c3cbc5f254cda348
allierc <allierc@janelia.hhmi.org>
2024-01-13 20:27:34 -0500

    Training
    wave HR

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -3811,9 +3811,9 @@ if __name__ == '__main__':
                 return torch.remainder(D - .5, 1.0) - .5
 
         ratio = 1
-        data_generate(model_config, bVisu=True, bStyle='color', alpha=0.2, bErase=True, bLoad_p=False, step=model_config['nframes']//4, ratio=ratio, scenario='none')
+        # data_generate(model_config, bVisu=True, bStyle='color', alpha=0.2, bErase=True, bLoad_p=False, step=model_config['nframes']//4, ratio=ratio, scenario='none')
         data_train(model_config,model_embedding)
-        data_plot(model_config, epoch=-1, bPrint=True, best_model=4, kmeans_input=model_config['kmeans_input'])
+        # data_plot(model_config, epoch=-1, bPrint=True, best_model=4, kmeans_input=model_config['kmeans_input'])
         # data_test(model_config, bVisu=True, bPrint=True, best_model=20, bDetails=False, step=10)
 
         # data_train_shrofflab_celegans(model_config)
