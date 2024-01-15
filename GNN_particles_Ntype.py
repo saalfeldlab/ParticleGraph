71c976eeecab207b66d7137fdb2786904626978c
allierc <allierc@janelia.hhmi.org>
2024-01-14 20:43:36 -0500

    Training
    boids_16_HR    boids_16_HR2

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -1352,7 +1352,7 @@ def data_generate(model_config, bVisu=True, bStyle='color', bErase=False, bLoad_
                     mask = mask[:, 0:1]
                     with torch.no_grad():
                         pred = model_mesh(dataset_mesh)
-                        H1[mask] += pred[mask] * delta_t
+                        H1 += pred * delta_t
 
                     if (model_config['model'] == 'RD_RPS_Mesh'):
 
@@ -3768,7 +3768,7 @@ if __name__ == '__main__':
     # config_list = ['config_wave_testA']
 
     # Test plotting figures paper
-    config_list = ['config_RD_RPS'] #, 'config_boids_16_HR'] #['config_RD_RPS','config_RD_RPS_05','config_RD_RPS_025'] #['config_RD_FitzHugh_Nagumo'] # ['config_arbitrary_3', 'config_gravity_16', 'config_Coulomb_3', 'config_boids_16'] # ['config_arbitrary_3'] # ['config_RD_FitzHugh_Nagumo'] # ,
+    config_list = ['config_boids_16_HR','config_boids_16_HR2'] #, 'config_boids_16_HR'] #['config_RD_RPS','config_RD_RPS_05','config_RD_RPS_025'] #['config_RD_FitzHugh_Nagumo'] # ['config_arbitrary_3', 'config_gravity_16', 'config_Coulomb_3', 'config_boids_16'] # ['config_arbitrary_3'] # ['config_RD_FitzHugh_Nagumo'] # ,
 
     with open(f'./config/config_embedding.yaml', 'r') as file:
         model_config_embedding = yaml.safe_load(file)
@@ -3808,8 +3808,8 @@ if __name__ == '__main__':
                 return torch.remainder(D - .5, 1.0) - .5
 
         ratio = 1
-        data_generate(model_config, bVisu=True, bStyle='color', alpha=0.2, bErase=True, bLoad_p=False, step=20) # model_config['nframes']//8, ratio=ratio, scenario='none')
-        # data_train(model_config,model_embedding)
+        data_generate(model_config, bVisu=True, bStyle='color', alpha=0.2, bErase=True, bLoad_p=False, model_config['nframes']//20, ratio=ratio, scenario='none')
+        data_train(model_config,model_embedding)
         # data_plot(model_config, epoch=-1, bPrint=True, best_model=4, kmeans_input=model_config['kmeans_input'])
         # data_test(model_config, bVisu=True, bPrint=True, best_model=20, bDetails=False, step=10)
 
