a1a4f7d73e9f00d68126ca45a8f5b4c6b3b8a132
Michael Innerberger <innerbergerm@hhmi.org>
2024-02-14 17:42:04 -0500

    Update pydantic model parameters

--- src/ParticleGraph/config.py
+++ src/ParticleGraph/config.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel, ConfigDict, Field, field_validator, model_valida
 
 from ParticleGraph.config_manager import ConfigManager
 
-from ParticleGraph import (
-    GraphModel,
-)
+# from ParticleGraph import (
+#     GraphModel,
+# )
 
 
 # Sub-config schemas for ParticleGraph
@@ -13,20 +13,23 @@ from ParticleGraph import (
 class SimulationConfig(BaseModel):
     model_config = ConfigDict(extra='forbid')
     
-    p: list
+    params: list[float]
     radius: Annotated[float, Field(gt=0)]
     min_radius: Annotated[float, Field(ge=0)] = 0
-    c: list
+    diffusion_coefficients: list[float]
     n_particles: int = 1000
     n_particle_types: int = 5
     n_interactions: int = 5
+    n_nodes: int = 900
+    n_node_types: int = 4
+    has_cell_division: bool = False
     n_frames: int = 1000
     sigma: Optional[float]
     delta_t: float = 1
-    v_init: float = 0
-    boundary: Literal["periodic", "no"] = "periodic"
-    particle_value_map: Optional[str]
-    particle_type_map: Optional[str]
+    dpos_init: float = 0
+    boundary: Literal['periodic', 'no'] = 'periodic'
+    node_value_map: Optional[str]
+    node_type_map: Optional[str]
     beta: Optional[float]
     start_frame: int = 0
 
@@ -40,38 +43,47 @@ class GraphModelConfig(BaseModel):
     hidden_dim: int
     n_mp_layers: int
     aggr_type: str
+    mesh_aggr_type: str = 'add'
     embedding: int = 2
-    upgrade_type: str = "none"
+    update_type: Literal['linear', 'none'] = 'none'
     n_layers_update: int = 3
     hidden_dim_update: int = 64
 
-    def get_instance(self, **kwargs):
-        return GraphModel(**self.model_dump(), **kwargs)
+    # def get_instance(self, **kwargs):
+    #     return GraphModel(**self.model_dump(), **kwargs)
 
 
 class PlottingConfig(BaseModel):
     model_config = ConfigDict(extra='forbid')
 
-    colormap: str = "tab10"
-    arrow_length:  int = 10
+    colormap: str = 'tab10'
+    arrow_length: int = 10
 
 
 class TrainingConfig(BaseModel):
     model_config = ConfigDict(extra='forbid')
     
-    n_epochs: int = 500
+    n_epochs: int = 20
     batch_size: int = 1
+    small_init_batch_size: bool = True
 
     n_runs: int = 2
     clamp: float = 0
     pred_limit: float = 1.E+10
-    sparsity: str = "none"
+    sparsity: Literal['none', 'replace'] = 'none'
+
+    fix_cluster_embedding: bool = False
+    loss_weight: bool = False
+    learning_rate_start: float = 0.001
+    learning_rate_end: float = 0.0005
+    learning_rate_embedding_start: float = 0.001
+    learning_rate_embedding_end: float = 0.0005
 
     noise_level: float = 0
-    kmeans_input: str = "plot"
     data_augmentation: bool = True
+    cluster_method: Literal['kmeans_auto', 'distance_plot', 'distance_embedding', 'distance_both'] = 'distance_plot'
     
-    device: Annotated[str, Field(pattern=r'^(auto|cpu|cuda:\d+)$')] = "auto"
+    device: Annotated[str, Field(pattern=r'^(auto|cpu|cuda:\d+)$')] = 'auto'
 
 
 # Main config schema for ParticleGraph
@@ -79,7 +91,7 @@ class TrainingConfig(BaseModel):
 class ParticleGraphConfig(BaseModel):
     model_config = ConfigDict(extra='forbid')
 
-    description: Optional[str] = "ParticleGraph"
+    description: Optional[str] = 'ParticleGraph'
     simulation: SimulationConfig
     graph_model: GraphModelConfig
     plotting: PlottingConfig
