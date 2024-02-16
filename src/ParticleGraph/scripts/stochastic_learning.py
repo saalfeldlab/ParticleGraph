26db1a6007f1b5c7b83255570dbbe11a4a0b220f
Cedric Allier <allierc@janelia.hhmi.org>
2024-02-16 16:22:40 -0500

    stochastic learning

--- src/ParticleGraph/scripts/stochastic_learning.py
+++ src/ParticleGraph/scripts/stochastic_learning.py
@@ -4,7 +4,9 @@ import numpy as np
 from torch.distributions import Categorical
 from torch.distributions import Categorical
 import torch.nn as nn
-
+import matplotlib.pyplot as plt
+import matplotlib
+from tqdm import trange
 
 class DeepNormal(nn.Module):
 
@@ -53,6 +55,22 @@ def compute_loss(model, x, y):
     return torch.mean(neg_log_likelihood)
 
 
+class my_distrib(torch.nn.Module):
+
+    def __init__(self):
+        
+        super(my_distrib, self).__init__()
+
+        self.learned_distribution = nn.Parameter(torch.ones(100, dtype=torch.float32, device='cuda:0', requires_grad=True))
+
+    def forward(self):
+
+        normalized_distribution = torch.relu(self.learned_distribution)
+        normalized_distribution = normalized_distribution / torch.sum(normalized_distribution)
+
+        m = Categorical(normalized_distribution)
+        return m, normalized_distribution
+
 
 if __name__ == '__main__':
 
@@ -61,32 +79,92 @@ if __name__ == '__main__':
     print('')
 
 
-    probs = torch.rand(100)
-    probs = probs / torch.sum(probs)
-    probs = probs.to('cuda:0')
-    gt_sampler = Categorical(probs)
+    gt_distrib1 = torch.distributions.Normal(25, 5)
+    gt_distrib2 = torch.distributions.Normal(75, 5)
 
-    learned_distribution = torch.zeros(10, dtype=torch.float32, device='cuda:0', requires_grad=True)
-    with torch.no_grad():
-        learned_distribution = torch.rand(10, dtype=torch.float32, device='cuda:0')
-        learned_distribution = learned_distribution / torch.sum(learned_distribution)
+    prob1 = gt_distrib1.sample((200,))
+    prob2 = gt_distrib2.sample((200,))
+    probs = torch.cat((prob1, prob2), 0)
 
-    learned_distribution.requires_grad = True
-    m = Categorical(learned_distribution)
+    fig = plt.figure(figsize=(10, 10))
+    plt.hist(probs.detach().cpu().numpy(),150)
+    plt.show()
+
+    t = plt.hist(probs.detach().cpu().numpy(), 100, range=[0, 100])
+    t = torch.tensor(t[0],device='cuda:0')
+    t = t / torch.sum(t)
+
+    # matplotlib.use("Qt5Agg")
+    # fig = plt.figure(figsize=(10, 10))
+    # plt.scatter(range(100), t.detach().cpu().numpy(), c='r')
+
+    gt_distrib = Categorical(t)
+
+    md = my_distrib()    
+    optimizer = torch.optim.Adam(md.parameters(), lr=0.01)
+
+    # plot a normal distribution
+
+    for n in trange(10000):
     
-    optimizer = torch.optim.Adam([learned_distribution], lr=0.01)
-
-    for n in range(1000):
-
-        draw = torch.randint(0, 10, (1,), device='cuda:0')
-        loss = m.log_prob(draw.detach()) - gt_sampler.log_prob(draw.detach())
+        m, normalized_distribution = md()
+        draw = torch.randint(0, 100, (100,), device='cuda:0')
+        loss = (m.log_prob(draw.detach()) - gt_distrib.log_prob(draw.detach())).norm(2)
+        
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-        
+
+    # matplotlib.use("Qt5Agg")
+    fig = plt.figure(figsize=(10, 10))
+    plt.scatter(range(100), t.detach().cpu().numpy(), c='g',s=40)
+    plt.scatter (range(100), normalized_distribution.detach().cpu().numpy(), c='k',s=2)
+    plt.show()
+    # plt.scatter(range(100), probs.detach().cpu().numpy())
+    
 
 
-        print(f'loss: {loss.item()}')
+
+    # def update_policy(policy, states, actions, log_prob_actions, advantages, returns, optimizer, ppo_steps, ppo_clip):
+    #     total_policy_loss = 0
+    #
+    #
+    # total_value_loss = 0
+    # states = states.detach()
+    # actions = actions.detach()
+    # log_prob_actions = log_prob_actions.detach()
+    # advantages = advantages.detach()
+    # returns = returns.detach()
+    #
+    # for _ in range(ppo_steps):  # get new log prob of actions for all input states
+    #
+    #     action_pred, value_pred = policy(states)
+    #     value_pred = value_pred.squeeze(-1)
+    #
+    #
+    #     action_prob = F.softmax(action_pred, dim=-1)
+    #
+    #
+    #     dist = distributions.Categorical(action_prob)  # new log prob using old actions
+    #
+    #
+    #     new_log_prob_actions = dist.log_prob(actions)
+    #
+    #
+    #     policy_ratio = (new_log_prob_actions - log_prob_actions).exp()
+    #     policy_loss_1 = policy_ratio * advantages
+    #     policy_loss_2 = torch.clamp(policy_ratio, min=1.0 - ppo_clip, max=1.0 + ppo_clip) * advantages
+    #
+    #
+    #     # here need to implement eligibility trace - really??! try
+    #     policy_loss = - torch.min(policy_loss_1, policy_loss_2).mean()
+    #     # update previous eligibility trace
+    #     # update eligibility trace        value_loss = F.smooth_l1_loss(returns, value_pred).mean()        optimizer.zero_grad()        policy_loss.backward()
+    #     value_loss.backward()
+    #     optimizer.step()
+    #     total_policy_loss += policy_loss.item()
+    #     total_value_loss += value_loss.item()
+    #     return total_policy_loss / ppo_steps, total_value_loss / ppo_steps
 
 
         
