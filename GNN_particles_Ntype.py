987304412336027b2f9f0cb9abac3b5d67845cc1
allierc <allierc@janelia.hhmi.org>
2024-02-18 08:44:44 -0500

    launch training Coulomb_3

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -1146,6 +1146,6 @@ if __name__ == '__main__':
 
         cmap = CustomColorMap(config=config)  # create colormap for given model_config
 
-        # data_generate(config, device=device, visualize=True, style='color', alpha=1, erase=True, step=1) # config.simulation.n_frames // 100)
+        data_generate(config, device=device, visualize=True, style='color', alpha=1, erase=True, step=1) # config.simulation.n_frames // 100)
         data_train(config)
         # data_test(config, visualize=True, verbose=True, best_model=20, step=config.simulation.n_frames // 50, ratio=1)
