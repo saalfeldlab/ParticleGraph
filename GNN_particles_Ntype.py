3b937ecd4f16847dd33d8abb1b97619b5a88f233
allierc <allierc@janelia.hhmi.org>
2024-02-25 18:49:49 -0500

    test wave_noise

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -646,6 +646,8 @@ def data_train(config):
 
                 if has_mesh:
                     x_mesh = x_mesh_list[run][k].clone().detach()
+                    if train_config.noise_level > 0:
+                        x_mesh[:, 6:7] = x_mesh[:, 6:7] + train_config.noise_level * torch.randn_like(x_mesh[:, 6:7])
                     dataset = data.Data(x=x_mesh, edge_index=edge_index_mesh, edge_attr=edge_weight_mesh, device=device)
                     dataset_batch.append(dataset)
                     y = y_mesh_list[run][k].clone().detach() / hnorm
@@ -1257,7 +1259,7 @@ if __name__ == '__main__':
     print('version 0.2.0 240111')
     print('')
 
-    config_list = ['boids_16_division'] # ['arbitrary_16', 'gravity_16', 'boids_16', 'Coulomb_3']    #['wave_e'] #['wave_a','wave_b','wave_c','wave_d'] ['RD_RPS'] #
+    config_list = ['wave_noise'] # ['arbitrary_16', 'gravity_16', 'boids_16', 'Coulomb_3']    #['wave_e'] #['wave_a','wave_b','wave_c','wave_d'] ['RD_RPS'] #
 
     for config_file in config_list:
 
@@ -1270,7 +1272,7 @@ if __name__ == '__main__':
 
         cmap = CustomColorMap(config=config)  # create colormap for given model_config
 
-        # data_generate(config, device=device, visualize=False , style='color', alpha=1, erase=True, step=20) # config.simulation.n_frames // 400)
+        data_generate(config, device=device, visualize=False , style='color', alpha=1, erase=True, step=20) # config.simulation.n_frames // 400)
         data_train(config)
         # data_test(config, visualize=True, verbose=True, best_model=17, step=20) #config.simulation.n_frames // 400)
 
