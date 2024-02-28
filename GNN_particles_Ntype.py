16e2de1822c06a050598842148c2d0ded98317dd
allierc <allierc@janelia.hhmi.org>
2024-02-28 18:24:12 -0500

    update training division

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -1418,7 +1418,7 @@ if __name__ == '__main__':
 
         cmap = CustomColorMap(config=config)  # create colormap for given model_config
 
-        # data_generate(config, device=device, visualize=False , style='color', alpha=1, erase=True, step=config.simulation.n_frames // 400, bSave=True)
+        data_generate(config, device=device, visualize=True , style='color', alpha=1, erase=True, step=config.simulation.n_frames // 400, bSave=True)
         data_train(config)
         # data_train_clock(config)
         # data_test(config, visualize=True, verbose=True, best_model=20, step=config.simulation.n_frames // 400)
