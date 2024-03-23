8dae327f880a6322479bf3795741dd93d65eb745
allierc <allierc@janelia.hhmi.org>
2024-03-23 10:38:18 -0400

    arbitrary
    grid from 10 to 200 frames

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -1379,7 +1379,7 @@ def data_test(config, visualize=False, verbose=True, best_model=20, step=5, rati
 
 if __name__ == '__main__':
 
-    config_list = ['arbitrary_3_2400','arbitrary_3_1200','arbitrary_3_600','arbitrary_3_300','arbitrary_3_90','arbitrary_3_30']
+    config_list = ['arbitrary_3_10_frames', 'arbitrary_3_25_frames', 'arbitrary_3_50_frames', 'arbitrary_3_100_frames', 'arbitrary_3_200_frames']
 
     for config_file in config_list:
         # Load parameters from config file
