fix rotation issue particle_field

src/ParticleGraph/generators/graph_data_generator.py
@@ -681,8 +681,9 @@ def data_generate_particle_field(config, visualize=True, run_vizualized=0, style
 
                 if 'video' in simulation_config.node_value_map:
                     im = imread(f"graphs_data/{simulation_config.node_value_map}") # / 255 * 5000
-                    im = np.fliplr(im)
-                    im = np.reshape(im[it], (n_nodes_per_axis * n_nodes_per_axis))
+                    im = im[it].squeeze()
+                    im = np.rot90(im,3)
+                    im = np.reshape(im, (n_nodes_per_axis * n_nodes_per_axis))
                     H1_mesh[:, 0:1] = torch.tensor(im[:,None], dtype=torch.float32, device=device)
                 else:
                     H1_mesh = rotate_init_mesh(it, config, device=device)
@@ -854,7 +855,7 @@ def data_generate_particle_field(config, visualize=True, run_vizualized=0, style
 
                 if 'color' in style:
 
-                    # matplotlib.use("Qt5Agg")
+                    matplotlib.use("Qt5Agg")
                     matplotlib.rcParams['savefig.pad_inches'] = 0
                     fig = plt.figure(figsize=(12, 12))
                     ax = fig.add_subplot(1, 1, 1)
@@ -871,56 +872,10 @@ def data_generate_particle_field(config, visualize=True, run_vizualized=0, style
                     # ax.get_xaxis().set_visible(False)
                     # ax.get_yaxis().set_visible(False)
                     # plt.autoscale(tight=True)
-                    if has_mesh:
-                        pts = x_mesh[:, 1:3].detach().cpu().numpy()
-                        tri = Delaunay(pts)
-                        colors = torch.sum(x_mesh[tri.simplices, 6], dim=1) / 3.0
-                        match model_config.mesh_model_name:
-                            case 'DiffMesh':
-                                plt.tripcolor(pts[:, 0], pts[:, 1], tri.simplices.copy(),
-                                              facecolors=colors.detach().cpu().numpy(), vmin=0, vmax=1000)
-                            case 'WaveMesh':
-                                plt.tripcolor(pts[:, 0], pts[:, 1], tri.simplices.copy(),
-                                              facecolors=colors.detach().cpu().numpy(), vmin=-1000, vmax=1000)
-                            case 'RD_Gray_Scott_Mesh':
-                                fig = plt.figure(figsize=(12, 6))
-                                ax = fig.add_subplot(1, 2, 1)
-                                colors = torch.sum(x[tri.simplices, 6], dim=1) / 3.0
-                                plt.tripcolor(pts[:, 0], pts[:, 1], tri.simplices.copy(),
-                                              facecolors=colors.detach().cpu().numpy(), vmin=0, vmax=1)
-                                plt.xticks([])
-                                plt.yticks([])
-                                plt.axis('off')
-                                ax = fig.add_subplot(1, 2, 2)
-                                colors = torch.sum(x[tri.simplices, 7], dim=1) / 3.0
-                                plt.tripcolor(pts[:, 0], pts[:, 1], tri.simplices.copy(),
-                                              facecolors=colors.detach().cpu().numpy(), vmin=0, vmax=1)
-                                plt.xticks([])
-                                plt.yticks([])
-                                plt.axis('off')
-                            case 'RD_RPS_Mesh':
-                                fig = plt.figure(figsize=(12, 12))
-                                H1_IM = torch.reshape(H1, (100, 100, 3))
-                                plt.imshow(H1_IM.detach().cpu().numpy(), vmin=0, vmax=1)
-                                plt.xticks([])
-                                plt.yticks([])
-                                plt.axis('off')
-                    else:
-                        s_p = 100
-                        if simulation_config.has_cell_division:
-                            s_p = 25
-                        if False:  # config.simulation.non_discrete_level>0:
-                            plt.scatter(to_numpy(x[:, 1]), to_numpy(x[:, 2]), s=s_p, color='k')
-                        else:
-                            for n in range(n_particle_types):
-                                plt.scatter(to_numpy(x[index_particles[n], 2]), to_numpy(x[index_particles[n], 1]),
-                                            s=s_p, color=cmap.color(n))
-                        if training_config.particle_dropout > 0:
-                            plt.scatter(x[inv_particle_dropout_mask, 1].detach().cpu().numpy(),
-                                        x[inv_particle_dropout_mask, 2].detach().cpu().numpy(), s=25, color='k',
-                                        alpha=0.75)
-                            plt.plot(x[inv_particle_dropout_mask, 1].detach().cpu().numpy(),
-                                     x[inv_particle_dropout_mask, 2].detach().cpu().numpy(), '+', color='w')
+                    s_p = 100
+                    for n in range(n_particle_types):
+                            plt.scatter(to_numpy(x[index_particles[n], 2]), to_numpy(x[index_particles[n], 1]),
+                                        s=s_p, color=cmap.color(n))
                     plt.xlim([0,1])
                     plt.ylim([0,1])
                     # plt.xlim([-2,2])
@@ -941,6 +896,48 @@ def data_generate_particle_field(config, visualize=True, run_vizualized=0, style
                     plt.tight_layout()
                     plt.savefig(f"graphs_data/graphs_{dataset_name}/generated_data/Fig_{run}_{it}.jpg", dpi=42.675)
                     plt.close()
+
+                    matplotlib.rcParams['savefig.pad_inches'] = 0
+
+
+
+                    fig = plt.figure(figsize=(12, 12))
+                    ax = fig.add_subplot(1, 1, 1)
+                    # ax.xaxis.get_major_formatter()._usetex = False
+                    # ax.yaxis.get_major_formatter()._usetex = False
+                    ax.xaxis.set_major_locator(plt.MaxNLocator(3))
+                    ax.yaxis.set_major_locator(plt.MaxNLocator(3))
+                    ax.xaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+                    ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+                    # if (has_mesh | (simulation_config.boundary == 'periodic')):
+                    #     ax = plt.axes([0, 0, 1, 1], frameon=False)
+                    # else:
+                    #     ax = plt.axes([-2, -2, 2, 2], frameon=False)
+                    # ax.get_xaxis().set_visible(False)
+                    # ax.get_yaxis().set_visible(False)
+                    # plt.autoscale(tight=True)
+                    s_p = 100
+                    plt.scatter(to_numpy(x_mesh[0:n_nodes, 2]), to_numpy(x_mesh[0:n_nodes, 1]), c=to_numpy(x_mesh[0:n_nodes, 6]))
+                    plt.xlim([0,1])
+                    plt.ylim([0,1])
+                    # plt.xlim([-2,2])
+                    # plt.ylim([-2,2])
+                    if 'latex' in style:
+                        plt.xlabel(r'$x$', fontsize=64)
+                        plt.ylabel(r'$y$', fontsize=64)
+                        plt.xticks(fontsize=32.0)
+                        plt.yticks(fontsize=32.0)
+                    elif 'frame' in style:
+                        plt.xlabel('x', fontsize=64)
+                        plt.ylabel('y', fontsize=64)
+                        plt.xticks(fontsize=32.0)
+                        plt.yticks(fontsize=32.0)
+                    else:
+                        plt.xticks([])
+                        plt.yticks([])
+                    plt.tight_layout()
+
+
 
                     if False:  # not(has_mesh):
                         fig = plt.figure(figsize=(12, 12))

src/ParticleGraph/models/graph_trainer.py
@@ -626,16 +626,14 @@ def data_train_mesh(config, config_file, device):
 
         # matplotlib.use("Qt5Agg")
         fig = plt.figure(figsize=(22, 4))
-        # white background
-        # plt.style.use('classic')
 
-        ax = fig.add_subplot(1, 6, 1)
+        ax = fig.add_subplot(1, 5, 1)
         plt.plot(list_loss, color='k')
         plt.xlim([0, n_epochs])
         plt.ylabel('Loss', fontsize=12)
         plt.xlabel('Epochs', fontsize=12)
 
-        ax = fig.add_subplot(1, 6, 2)
+        ax = fig.add_subplot(1, 5, 2)
         embedding = get_embedding(model.a, 1)
         for n in range(n_node_types):
             plt.scatter(embedding[index_nodes[n], 0],
@@ -645,7 +643,7 @@ def data_train_mesh(config, config_file, device):
 
         if (simulation_config.n_interactions < 100):
 
-            ax = fig.add_subplot(1, 6, 3)
+            ax = fig.add_subplot(1, 5, 3)
             func_list = []
             popt_list = []
             for n in range(n_nodes):
@@ -681,6 +679,7 @@ def data_train_mesh(config, config_file, device):
             func_list = torch.stack(func_list)
             coeff_norm = to_numpy(func_list)
             popt_list = np.array(popt_list)
+
             if 'RD_RPS_Mesh' in model_config.mesh_model_name:
                 trans = umap.UMAP(n_neighbors=500, n_components=2, transform_queue_size=0).fit(coeff_norm)
                 proj_interaction = trans.transform(coeff_norm)
@@ -688,52 +687,22 @@ def data_train_mesh(config, config_file, device):
                 proj_interaction = popt_list
                 proj_interaction[:, 1] = proj_interaction[:, 0]
 
-            ax = fig.add_subplot(1, 6, 4)
-            match train_config.cluster_method:
-                case 'kmeans_auto_plot':
-                    labels, n_clusters = embedding_cluster.get(proj_interaction, 'kmeans_auto')
-                case 'kmeans_auto_embedding':
-                    labels, n_clusters = embedding_cluster.get(embedding, 'kmeans_auto')
-                    proj_interaction = embedding
-                case 'distance_plot':
-                    labels, n_clusters = embedding_cluster.get(proj_interaction, 'distance')
-                case 'distance_embedding':
-                    labels, n_clusters = embedding_cluster.get(embedding, 'distance', thresh=1.5)
-                    proj_interaction = embedding
-                case 'distance_both':
-                    new_projection = np.concatenate((proj_interaction, embedding), axis=-1)
-                    labels, n_clusters = embedding_cluster.get(new_projection, 'distance')
-            for n in range(n_clusters):
-                pos = np.argwhere(labels == n)
-                pos = np.array(pos)
-                if pos.size > 0:
-                    plt.scatter(proj_interaction[pos, 0], proj_interaction[pos, 1], color=cmap.color(n), s=5)
-            label_list = []
-            for n in range(n_node_types):
-                tmp = labels[index_nodes[n]]
-                label_list.append(np.round(np.median(tmp)))
-            label_list = np.array(label_list)
+            labels, n_clusters, new_labels = sparsify_cluster(train_config.cluster_method, proj_interaction, embedding, cluster_distance_threshold, index_particles, n_particle_types, embedding_cluster)
 
-            plt.xlabel('proj 0', fontsize=12)
-            plt.ylabel('proj 1', fontsize=12)
-            plt.text(0., 1.1, f'Nclusters: {n_clusters}', ha='left', va='top', transform=ax.transAxes)
-
-            ax = fig.add_subplot(1, 6, 5)
-            new_labels = labels.copy()
-            for n in range(n_node_types):
-                new_labels[labels == label_list[n]] = n
-                pos = np.argwhere(labels == label_list[n])
-                pos = np.array(pos)
-                if pos.size > 0:
-                    plt.scatter(proj_interaction[pos, 0], proj_interaction[pos, 1],
-                                color=cmap.color(n), s=0.1)
             Accuracy = metrics.accuracy_score(to_numpy(type_list), new_labels)
-            plt.text(0, 1.1, f'Accuracy: {np.round(Accuracy, 3)}', ha='left', va='top', transform=ax.transAxes,
-                     fontsize=10)
             print(f'Accuracy: {np.round(Accuracy, 3)}   n_clusters: {n_clusters}')
             logger.info(f'Accuracy: {np.round(Accuracy, 3)}    n_clusters: {n_clusters}')
 
-            ax = fig.add_subplot(1, 6, 6)
+            ax = fig.add_subplot(1, 5, 4)
+            for n in np.unique(new_labels):
+                pos = np.array(np.argwhere(new_labels == n).squeeze().astype(int))
+                if pos.size > 0:
+                    plt.scatter(proj_interaction[pos, 0], proj_interaction[pos, 1], s=5)
+            plt.xlabel('proj 0', fontsize=12)
+            plt.ylabel('proj 1', fontsize=12)
+            plt.text(0, 1.1, f'Accuracy: {np.round(Accuracy, 3)},  {n_clusters} clusters', ha='left', va='top', transform=ax.transAxes,fontsize=10)
+
+            ax = fig.add_subplot(1, 5, 5)
             model_a_ = model.a[1].clone().detach()
             for n in range(n_clusters):
                 pos = np.argwhere(labels == n).squeeze().astype(int)
@@ -743,12 +712,8 @@ def data_train_mesh(config, config_file, device):
                     median_center = torch.median(median_center, dim=0).values
                     plt.scatter(to_numpy(model_a_[pos, 0]), to_numpy(model_a_[pos, 1]), s=1, c='r', alpha=0.25)
                     model_a_[pos, :] = median_center
-                    plt.scatter(to_numpy(model_a_[pos, 0]), to_numpy(model_a_[pos, 1]), s=1, c='k')
-            for n in np.unique(new_labels):
-                pos = np.argwhere(new_labels == n).squeeze().astype(int)
-                pos = np.array(pos)
-                if pos.size > 0:
-                    plt.scatter(to_numpy(model_a_[pos, 0]), to_numpy(model_a_[pos, 1]), color='k', s=5)
+                    plt.scatter(to_numpy(model_a_[pos, 0]), to_numpy(model_a_[pos, 1]), s=10, c='k')
+
             plt.xlabel('ai0', fontsize=12)
             plt.ylabel('ai1', fontsize=12)
             plt.xticks(fontsize=10.0)