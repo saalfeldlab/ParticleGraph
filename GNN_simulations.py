b4fb7dc4ae088dad98faf30ed8eaa530d84748db
cedric <allierc@janelia.hhmi.org>
2024-06-24 12:45:22 -0400

    GNN_simulations

--- GNN_simulations.py
+++ GNN_simulations.py
@@ -49,11 +49,11 @@ if __name__ == '__main__':
 
     # matplotlib.use("Qt5Agg")
 
-    config_list = ['boids_16_256_division', 'arbitrary_3_sequence', 'arbitrary_3_division', 'arbitrary_3_field_video_bison', 'arbitrary_sequence', 'arbitrary_3', 'arbitrary_3_continuous', 'arbitrary_3_3', 'arbitrary_16', 'arbitrary_32', 'arbitrary_64']
+    config_list = ['wave_slit', 'RD_RPS_2', 'gravity_16', 'boids_16_256', 'Coulomb_3', 'arbitrary_3', 'arbitrary_3_3', 'arbitrary_16', 'arbitrary_32', 'arbitrary_64', 'boids_16_256_division', 'arbitrary_3_sequence', 'arbitrary_3_field_video_bison']
 
 
     for config_file in config_list:
         config = ParticleGraphConfig.from_yaml(f'./config/{config_file}.yaml')
 
-        data_generate(config, device=device, visualize=True, run_vizualized=0, style='frame color', alpha=1, erase=True, bSave=True, step=config.simulation.n_frames // 100)
+        data_generate(config, device=device, visualize=True, run_vizualized=0, style='frame color', alpha=1, erase=True, bSave=True, step=config.simulation.n_frames // 200)
 
