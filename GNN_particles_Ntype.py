82d33e7611aa19736c74731507b1a73d22b37884
Kabilan Prasanna <kabilanprasanna@gmail.com>
2024-07-16 16:13:42 -0400

    voronoi color

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -32,7 +32,8 @@ from ParticleGraph.utils import *
 
 if __name__ == '__main__':
 
-    config_list = ['boids_16_division_model_2_Voronoi']
+    #config_list = ['boids_16_division_model_2_Voronoi_a', 'boids_16_division_model_2_Voronoi_b', 'boids_16_division_model_2_Voronoi_c']
+    config_list = ["boids_16_division_model_2_Voronoi"]
 
 
     for config_file in config_list:
@@ -43,7 +44,7 @@ if __name__ == '__main__':
         device = set_device(config.training.device)
         print(f'device {device}')
 
-        data_generate(config, device=device, visualize=True, run_vizualized=0, style='voronoi frame', alpha=1, erase=True, bSave=True, step=config.simulation.n_frames // 400)
+        data_generate(config, device=device, visualize=True, run_vizualized=0, style='voronoi frame', alpha=1, erase=True, bSave=True, step=config.simulation.n_frames // 1000)
         # data_train(config, config_file, device)
         # data_test(config=config, config_file=config_file, visualize=True, style='latex frame color', verbose=False, best_model=20, run=1, step=config.simulation.n_frames // 3, test_simulation=False, sample_embedding=False, device=device)    # config.simulation.n_frames // 7
 
