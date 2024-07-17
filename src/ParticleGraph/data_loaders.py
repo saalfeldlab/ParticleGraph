ad0f32d2ec0685809fadb1a02caf5588d2843162
Michael Innerberger <innerbergerm@hhmi.org>
2024-07-17 10:33:34 -0400

    Use own load function to load C elegans data

--- src/ParticleGraph/data_loaders.py
+++ src/ParticleGraph/data_loaders.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 from typing import Dict, Tuple
 
 import astropy.units as u
+import numpy as np
 import pandas as pd
 from astropy.units import Unit
 from tqdm import trange
@@ -163,7 +164,7 @@ def load_shrofflab_celegans(
     Load the Shrofflab C. elegans data from a CSV file and convert it to a PyTorch tensor.
 
     :param file_path: The path to the CSV file.
-    :param replace_missing_cpm: If not None, replace missing cpm values with this value.
+    :param replace_missing_cpm: If not None, replace missing cpm values (NaN) with this value.
     :param device: The PyTorch device to allocate the tensors on.
     :return: A tuple consisting of:
      * A list of PyTorch tensors containing the loaded data for each time point.
@@ -176,28 +177,33 @@ def load_shrofflab_celegans(
     # Load the data from the CSV file and clean it a bit:
     # - drop rows with missing time values (occurs only at the end of the data)
     # - fill missing cpm values (don't interpolate because data is missing at the beginning or end)
-    print(f"Loading data from {file_path}...")
-    dtypes = {"time": np.float32, "x": np.float32, "y": np.float32,
-              "z": np.float32, "cell": str, "log10 mean cpm": str}
-    data = pd.read_csv(file_path, dtype=dtypes)
-    print(f"Loaded {data.shape[0]} rows of data, dropping rows with missing time values...")
-    data.dropna(subset=["time"], inplace=True)
-    print(f"Remaining: {data.shape[0]} rows")
+    column_descriptors = {
+        "x": CsvDescriptor(filename=file_path, column_name="x", type=np.float32, unit=u.micrometer),
+        "y": CsvDescriptor(filename=file_path, column_name="y", type=np.float32, unit=u.micrometer),
+        "z": CsvDescriptor(filename=file_path, column_name="z", type=np.float32, unit=u.micrometer),
+        "t": CsvDescriptor(filename=file_path, column_name="time", type=np.float32, unit=u.day),
+        "cell": CsvDescriptor(filename=file_path, column_name="cell", type=str, unit=u.dimensionless_unscaled),
+        "cpm": CsvDescriptor(filename=file_path, column_name="log10 mean cpm", type=np.float32, unit=u.dimensionless_unscaled),
+    }
+    raw_data = load_csv_from_descriptors(column_descriptors)
+    print(f"Loaded {raw_data.shape[0]} rows of data, dropping rows with missing time values...")
+    raw_data.dropna(subset=["t"], inplace=True)
+    print(f"Remaining: {raw_data.shape[0]} rows")
     if replace_missing_cpm is not None:
         print(f"Filling missing cpm values with {replace_missing_cpm}...")
-        data.fillna(replace_missing_cpm, inplace=True)
+        raw_data.fillna(replace_missing_cpm, inplace=True)
 
     # Find the indices where the data for each cell begins (time resets)
-    time_reset = np.where(np.diff(data["time"]) < 0)[0] + 1
-    timeseries_boundaries = np.hstack([0, time_reset, data.shape[0]])
+    time_reset = np.where(np.diff(raw_data["t"]) < 0)[0] + 1
+    timeseries_boundaries = np.hstack([0, time_reset, raw_data.shape[0]])
     n_timepoints = np.diff(timeseries_boundaries).astype(int)
     n_normal_timepoints = np.median(n_timepoints).astype(int)
-    start_time, end_time = np.min(data["time"]), np.max(data["time"]) + 1
+    start_time, end_time = np.min(raw_data["t"]), np.max(raw_data["t"]) + 1
     n_cells = len(n_timepoints)
 
     # Sanity checks to make sure the data is not too bad
     n_normal_data = np.count_nonzero(n_timepoints == n_normal_timepoints)
-    cell_names = data["cell"].values[timeseries_boundaries[:-1]]
+    cell_names = raw_data["cell"].values[timeseries_boundaries[:-1]]
     if (end_time - start_time) != n_normal_timepoints:
         raise ValueError("The time series are not part of the same timeframe.")
     if n_normal_data < 0.5 * n_cells:
@@ -208,14 +214,14 @@ def load_shrofflab_celegans(
         print(f"Warning: incomplete time series data for {abnormal_cells}")
 
     # Put values into a 3D tensor
-    relevant_fields = ["x", "y", "z", "log10 mean cpm"]
+    relevant_fields = ["x", "y", "z", "cpm"]
     shape = (n_cells, n_normal_timepoints, len(relevant_fields))
     tensor = np.nan * np.ones((shape[0] * shape[1], shape[2]))
-    time_idx = (data["time"].values - start_time).astype(int)
+    time_idx = (raw_data["t"].values - start_time).astype(int)
     cell_idx = np.repeat(np.arange(n_cells), n_timepoints)
     idx = np.ravel_multi_index((cell_idx, time_idx), (n_cells, n_normal_timepoints))
     for i, name in enumerate(relevant_fields):
-        tensor[idx, i] = data[name].values
+        tensor[idx, i] = raw_data[name].values
     tensor = np.transpose(tensor.reshape(shape), (1, 0, 2))
 
     # Compute the time derivatives and concatenate such that columns correspond to:
