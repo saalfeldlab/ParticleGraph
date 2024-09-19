b7b0abe1a71f737d311fc027f313c7a55f2f6965
cedric <allierc@janelia.hhmi.org>
2024-09-19 12:48:56 -0400

    fixing arbitrary bisons

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -11,20 +11,24 @@ if __name__ == '__main__':
     except:
         pass
 
-    # config_list = ['arbitrary_3', 'arbitrary_3_continuous',
+    # config_list = ['arbitrary_3', 'arbitrary_3_continuous', 'arbitrary_3_3', 'arbitrary_16', 'arbitrary_32', 'arbitrary_64',
 
-    config_list = ['arbitrary_3_3', 'arbitrary_16', 'arbitrary_32',
-                   'arbitrary_64', 'arbitrary_3_field_video_bison_quad', 'arbitrary_16', 'arbitrary_16_noise_0_3',
+    config_list = ['arbitrary_3_field_video_bison_quad', 'arbitrary_16', 'arbitrary_16_noise_0_3',
                    'arbitrary_16_noise_0_4', 'arbitrary_16_noise_0_5', 'arbitrary_3_dropout_30', 'arbitrary_3_dropout_10',
                    'arbitrary_3_dropout_10', 'arbitrary_3_dropout_10_no_ghost',
-                   'arbitrary_3_field_boats', 'gravity_16', 'gravity_continuous',
-                   'Coulomb_3_256', 'gravity_16_noise_0_4', 'Coulomb_3_noise_0_4', 'Coulomb_3_noise_0_3',
-                   'gravity_16_noise_0_3', 'gravity_16_dropout_10', 'gravity_16_dropout_30',
-                   'Coulomb_3_dropout_10_no_ghost', 'Coulomb_3_dropout_10', 'boids_16_256', 'boids_32_256',
-                   'boids_64_256', 'boids_16_noise_0_3', 'boids_16_noise_0_4', 'boids_16_dropout_10',
-                   'boids_16_dropout_10_no_ghost', 'wave_slit_ter', 'wave_boat_ter', 'RD_RPS','signal_N_100_2_a']
+                   'arbitrary_3_field_boats', 'gravity_16', 'gravity_continuous']
+
+    # config_list = ['Coulomb_3_256', 'gravity_16_noise_0_4', 'Coulomb_3_noise_0_4', 'Coulomb_3_noise_0_3',
+    #                'gravity_16_noise_0_3', 'gravity_16_dropout_10', 'gravity_16_dropout_30',
+    #                'Coulomb_3_dropout_10_no_ghost', 'Coulomb_3_dropout_10']
+
+    # config_list = ['boids_16_256', 'boids_32_256', 'boids_64_256', 'boids_16_noise_0_3', 'boids_16_noise_0_4', 'boids_16_dropout_10',
+    #                'boids_16_dropout_10_no_ghost', 'wave_slit_ter', 'wave_boat_ter', 'RD_RPS','signal_N_100_2_a']
 
     # config_list = ['arbitrary_3_3']
+
+    # dataset = xr.open_zarr('groups/saalfeld/home/allierc/signaling/Janne/model_0092_000_weights.zarr')
+    # trained_weights = dataset[’trained’]
 
 
     for config_file in config_list:
