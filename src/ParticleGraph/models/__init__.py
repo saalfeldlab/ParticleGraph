351c32cd1c0181f200b3e5ce9262af47231d7178
cedric <allierc@janelia.hhmi.org>
2024-12-16 10:41:57 -0500

    remove load_WaterRampsWall

--- src/ParticleGraph/models/__init__.py
+++ src/ParticleGraph/models/__init__.py
@@ -1,5 +1,6 @@
 from .Interaction_Particle import Interaction_Particle
 from .Interaction_Falling_Water import Interaction_Falling_Water
+from .Interaction_Falling_Water_Wall import Interaction_Falling_Water_Wall
 from .Interaction_Planet import Interaction_Planet
 from .Interaction_Planet2 import Interaction_Planet2
 from .Interaction_Agent import Interaction_Agent
@@ -22,6 +23,6 @@ from .utils import KoLeoLoss, get_embedding, get_embedding_time_series, choose_t
 from .Gumbel import sample_gumbel, gumbel_softmax_sample, gumbel_softmax
 from .WBI_Communication import WBI_Communication
 
-__all__ = [graph_trainer, Interaction_Agent, Interaction_Particle, Interaction_Falling_Water, Interaction_Cell, Cell_Area, Interaction_Planet, Interaction_Planet2, Interaction_Particle_Field, Interaction_Mouse_Field, Smooth_Particle, Siren_Network, Signal_Propagation,
+__all__ = [graph_trainer, Interaction_Agent, Interaction_Particle, Interaction_Falling_Water, Interaction_Falling_Water_Wall, Interaction_Cell, Cell_Area, Interaction_Planet, Interaction_Planet2, Interaction_Particle_Field, Interaction_Mouse_Field, Smooth_Particle, Siren_Network, Signal_Propagation,
            Signal_Propagation2, Mesh_RPS, Mesh_RPS_bis, Mesh_Laplacian, Division_Predictor, Ghost_Particles, KoLeoLoss, get_embedding, get_embedding_time_series, choose_training_model, constant_batch_size,
            increasing_batch_size, set_trainable_parameters, set_trainable_division_parameters, plot_training, sample_gumbel, gumbel_softmax_sample, gumbel_softmax, WBI_Communication]
