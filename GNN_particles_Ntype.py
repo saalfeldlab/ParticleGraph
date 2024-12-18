469a0bf90b4c8d6e2a96e939b241e083d92329a0
allierc <allierc@janelia.hhmi.org>
2024-12-18 14:28:00 -0500

    remove smooth_particle

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -71,8 +71,8 @@ if __name__ == '__main__':
         if 'train' in task:
             data_train(config=config, config_file=config_file, erase=False, best_model=best_model, device=device)
         if 'test' in task:
-            data_test(config=config, config_file=config_file, visualize=True, style='black color', verbose=False, best_model='best', run=1, plot_data=False,
-                      test_simulation=False, sample_embedding=False, device=device, fixed=False, bounce=False, step=12) # config.simulation.n_frames // 200, )  arrow speed acc_learned   arrow speed acc_true
+            data_test(config=config, config_file=config_file, visualize=True, style='black color', verbose=False, best_model='best', run=2, plot_data=False,
+                      test_simulation=False, sample_embedding=False, device=device, fixed=False, bounce=False, step=4) # config.simulation.n_frames // 200, )  arrow speed acc_learned   arrow speed acc_true
 
 
 # bsub -n 4 -gpu "num=1" -q gpu_h100 "python GNN_particles_Ntype.py -o train falling_water_ramp_x1"
