0fa5cc1282d2b3a15c33de642d77c8bb9a4bf6fe
cedric <allierc@janelia.hhmi.org>
2025-03-05 11:20:27 -0500

    add siren to learn plasticity

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -56,9 +56,9 @@ if __name__ == '__main__':
             best_model = None
     else:
         task = 'train'
-        best_model = '60'
+        best_model = None
         # config_list = ['falling_water_ramp_x6_11']
-        config_list =['signal_N6_a29_1']
+        config_list =['signal_N6_a28_2']
         # config_list = ['arbitrary_3_test']
         # config_list =['wave_smooth_3']
         # config_list = ['signal_N2_e5']
--- src/ParticleGraph/models/graph_trainer.py
+++ src/ParticleGraph/models/graph_trainer.py
@@ -2283,10 +2283,17 @@ def data_train_synaptic2(config, erase, best_model, device):
     print('create models ...')
     model, bc_pos, bc_dpos = choose_training_model(model_config=config, device=device, projections=projections)
     if has_Siren:
-        model_f = Siren_Network(image_width=n_nodes_per_axis, in_features=model_config.input_size_nnr,
-                                out_features=model_config.output_size_nnr, hidden_features=model_config.hidden_dim_nnr,
-                                hidden_layers=model_config.n_layers_nnr, outermost_linear=True, device=device,
-                                first_omega_0=omega, hidden_omega_0=omega)
+
+        if 'Siren_short_term_plasticity' in field_type:
+            model_f = Siren(in_features=model_config.input_size_nnr, out_features=model_config.output_size_nnr,
+                            hidden_features=model_config.n_layers_nnr,
+                            hidden_layers=model_config.n_layers_nnr, first_omega_0=omega, hidden_omega_0=omega,
+                            outermost_linear=True)
+        else:
+            model_f = Siren_Network(image_width=n_nodes_per_axis, in_features=model_config.input_size_nnr,
+                                    out_features=model_config.output_size_nnr, hidden_features=model_config.hidden_dim_nnr,
+                                    hidden_layers=model_config.n_layers_nnr, outermost_linear=True, device=device,
+                                    first_omega_0=omega, hidden_omega_0=omega)
         model_f.to(device=device)
         optimizer_f = torch.optim.Adam(lr=train_config.learning_rate_NNR, params=model_f.parameters())
         model_f.train()
