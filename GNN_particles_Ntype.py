d3fbedf7fbd5f8fce77cebfec88467efc1a3b883
cedric <allierc@janelia.hhmi.org>
2025-04-15 09:30:10 -0400

    compute parallel batch

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -55,18 +55,16 @@ if __name__ == '__main__':
             best_model = None
     else:
 
-        task = 'test'
+        task = 'train'
         best_model = ''
         # config_list = ['falling_water_ramp_x6_11']
         # config_list = ['falling_water_ramp_x6_11_1']
-        config_list = ['multimaterial_8_2'] #,'multimaterial_8_3', 'multimaterial_8'] #, 'multimaterial_2', 'multimaterial_1', 'multimaterial_3', 'multimaterial_4']
+        # config_list = ['multimaterial_8', 'multimaterial_8_3','multimaterial_8_4', 'multimaterial_8_1', 'multimaterial_8_2', 'multimaterial_2_1', 'multimaterial_2_2', 'multimaterial_2_3', 'multimaterial_1', 'multimaterial_3', 'multimaterial_4']
         # config_list = ['falling_water_ramp_x6_11_1','falling_water_ramp_x6_11_2','falling_water_ramp_x6_11_3','falling_water_ramp_x6_11_4',
-        #         'falling_water_ramp_x6_11_5','falling_water_ramp_x6_11_6','falling_water_ramp_x6_11_7','falling_water_ramp_x6_11_8',
-        #         'falling_water_ramp_x6_11_9']
-
+        #         'falling_water_ramp_x6_11_5','falling_water_ramp_x6_11_6','falling_water_ramp_x6_11_7','falling_water_ramp_x6_11_8', 'falling_water_ramp_x6_11_9','falling_water_ramp_x6_11_10', 'falling_water_ramp_x6_11_11']
         # config_list = ['fluids_m14']
         # config_list = ['signal_N4_all_1']
-        # config_list = ['arbitrary_3_test']
+        config_list = ['arbitrary_3_test']
         # config_list = ['wave_smooth_3']
         # config_list = ['signal_N2_a38_1']
         # config_list = ['gravity_16']
@@ -92,7 +90,7 @@ if __name__ == '__main__':
         if 'train' in task:
             data_train(config=config, erase=False, best_model=best_model, device=device)
         if 'test' in task:
-            data_test(config=config, visualize=True, style='black color', verbose=False, best_model='best', run=17, test_mode='fixed_bounce_all_black', sample_embedding=False, step=4, device=device) # particle_of_interest=100,
+            data_test(config=config, visualize=True, style='black color', verbose=False, best_model='best', run=2, test_mode='fixed_bounce_all_black', sample_embedding=False, step=4, device=device) # particle_of_interest=100,
         if 'try_func' in task:
             try_func(max_radius=config.simulation.max_radius, device=device)
 
