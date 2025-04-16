45e98daec3bc6b1c60a388b5a8f12383013aca2a
cedric <allierc@janelia.hhmi.org>
2025-04-16 12:53:10 -0400

    update train particle batch_ratio

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
             best_model = None
     else:
 
-        task = 'train'
+        task = 'test'
         best_model = ''
         # config_list = ['falling_water_ramp_x6_11']
         # config_list = ['falling_water_ramp_x6_11_1']
@@ -91,7 +91,7 @@ if __name__ == '__main__':
         if 'train' in task:
             data_train(config=config, erase=False, best_model=best_model, device=device)
         if 'test' in task:
-            data_test(config=config, visualize=True, style='black color', verbose=False, best_model='best', run=17, test_mode='plot_data', sample_embedding=False, step=4, device=device) # particle_of_interest=100,
+            data_test(config=config, visualize=True, style='black color', verbose=False, best_model='best', run=1, test_mode='', sample_embedding=False, step=4, device=device) # particle_of_interest=100,
         if 'try_func' in task:
             try_func(max_radius=config.simulation.max_radius, device=device)
 
