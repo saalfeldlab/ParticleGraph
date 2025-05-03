0405c0b97dcf30ea65625e8493db613fd75a11c2
allierc <allierc@janelia.hhmi.org>
2025-05-03 11:53:58 +0200

    update coeff_sign constraint

--- src/ParticleGraph/models/graph_trainer.py
+++ src/ParticleGraph/models/graph_trainer.py
@@ -2631,10 +2631,21 @@ def data_train_synaptic2(config, erase, best_model, device):
                         loss = loss + torch.relu(msg0 - msg1).norm(2) * coeff_diff
 
                     if coeff_sign > 0:
+                        # W_sign = torch.sign(model.W)
+                        # for i in range(n_particles):
+                        #     if len(index_weight[i]) > 0:
+                        #         loss = loss + torch.std(W_sign[index_weight[i], i]) * coeff_sign
+
                         W_sign = torch.sign(model.W)
+                        loss_contribs = []
                         for i in range(n_particles):
-                            if len(index_weight[i]) > 0:
-                                loss = loss + torch.std(W_sign[index_weight[i], i]) * coeff_sign
+                            indices = index_weight[i]
+                            if len(indices) > 0:
+                                values = W_sign[indices, i]
+                                std = torch.std(values, unbiased=False)
+                                loss_contribs.append(std)
+                        if loss_contribs:
+                            loss = loss + torch.stack(loss_contribs).norm(2) * coeff_sign
 
                     if (model.update_type == 'generic') & (coeff_diff_update>0):
                         in_feature_update = torch.cat((torch.zeros((n_particles,1), device=device), model.a[:n_particles], msg0, torch.ones((n_particles,1), device=device)), dim=1)
