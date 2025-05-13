65c71facd4939be01ae2fbe9e91dfec67e3bef9b
cedric <allierc@janelia.hhmi.org>
2025-05-13 14:53:30 -0400

    add training cardio

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
         else:
             best_model = None
     else:
-        task = 'generate'
+        task = 'test'
         best_model = None
 
         # config_list = ['multimaterial_12_1','multimaterial_12_2','multimaterial_12_3','multimaterial_12_4','multimaterial_12_5',
@@ -88,8 +88,8 @@ if __name__ == '__main__':
             #     data_test(config=config, visualize=True, style='black color name', verbose=False, best_model='best',
             #               run=run_, test_mode='fixed_bounce_all', sample_embedding=False, step=4,
             #               device=device)  # particle_of_interest=100,
-            data_test(config=config, visualize=True, style='black color name', verbose=False, best_model='best', run=1, test_mode='', sample_embedding=False,
-                      step=4, device=device)  # particle_of_interest=100,  'fixed_bounce_all'
+            data_test(config=config, visualize=True, style='black color name', verbose=False, best_model='best', run=17, test_mode='fixed_bounce_all', sample_embedding=False,
+                      step=4, device=device)  # particle_of_interest=100,
 
 
 
