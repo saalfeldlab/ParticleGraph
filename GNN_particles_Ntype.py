56d07f39662d12ce5017e6de2a346eb1efbc9212
cedric <allierc@janelia.hhmi.org>
2025-06-03 09:45:20 -0400

    train m13_shuffle

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -55,11 +55,11 @@ if __name__ == '__main__':
         else:
             best_model = None
     else:
-        task = 'generate'
+        task = 'train'
         best_model = ''
         # config_list = ['falling_water_ramp_x6_11']
         # config_list = ['signal_N6_a29_1']
-        config_list = ['signal_N4_m12_shuffle']
+        config_list = ['signal_N4_m13_shuffle']
         # config_list = ['arbitrary_3_test']
         # config_list = ['wave_smooth_3']
         # config_list = ['signal_N2_e5']
