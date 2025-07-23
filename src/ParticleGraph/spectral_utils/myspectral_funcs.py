d791e213889f090b9e0abfd68963e2fe2d6f6e6e
Virginia Ruetten <ruettenv@janelia.hhmi.org>
2025-07-23 09:02:27 -0400

    add spectral functions (#14)

---
+++ src/ParticleGraph/spectral_utils/myspectral_funcs.py
@@ -0,0 +1,472 @@
+import numpy as np
+from numpy import fft as sp_fft
+import dask.array as da
+
+
+def make_chunks(x: np.ndarray, nperseg: int, noverlap: int):
+    """rechuncks data efficiently using strides"""
+    step = nperseg - noverlap
+    shape = x.shape[:-1] + ((x.shape[-1] - noverlap) // step, nperseg)
+    strides = x.strides[:-1] + (step * x.strides[-1], x.strides[-1])
+    result = np.lib.stride_tricks.as_strided(x, shape=shape, strides=strides)
+    return result  # result is N x K x T
+
+def _triage_segments(window, nperseg: int, input_length: int):
+    from scipy.signal.windows import get_window
+    import warnings
+
+    """
+    Parses window and nperseg arguments for spectrogram and _spectral_helper.
+    This is a helper function, not meant to be called externally.
+    Parameters
+    ----------
+    window : string, tuple, or ndarray # default is "hann"
+        If window is specified by a string or tuple and nperseg is not
+        specified, nperseg is set to the default of 256 and returns a window of
+        that length.
+        If instead the window is array_like and nperseg is not specified, then
+        nperseg is set to the length of the window. A ValueError is raised if
+        the user supplies both an array_like window and a value for nperseg but
+        nperseg does not equal the length of the window.
+    nperseg : int
+        Length of each segment
+    input_length: int
+        Length of input signal, i.e. x.shape[-1]. Used to test for errors.
+    Returns
+    -------
+    win : ndarray
+        window. If function was called with string or tuple than this will hold
+        the actual array used as a window.
+    nperseg : int
+        Length of each segment. If window is str or tuple, nperseg is set to
+        256. If window is array_like, nperseg is set to the length of the
+        window.
+    """
+    # parse window; if array like, then set nperseg = win.shape
+    if isinstance(window, str) or isinstance(window, tuple):
+        # if nperseg not specified
+        if nperseg is None:
+            nperseg = 256  # then change to default
+        if nperseg > input_length:
+            warnings.warn(
+                "nperseg = {0:d} is greater than input length "
+                " = {1:d}, using nperseg = {1:d}".format(nperseg, input_length)
+            )
+            nperseg = input_length
+        win = get_window(window, nperseg)  # return a 1d array
+    else:
+        win = np.asarray(window)
+        if len(win.shape) != 1:
+            raise ValueError("window must be 1-D")
+        if input_length < win.shape[-1]:
+            raise ValueError("window is longer than input signal")
+        if nperseg is None:
+            nperseg = win.shape[0]
+        elif nperseg is not None:
+            if nperseg != win.shape[0]:
+                raise ValueError(
+                    "value specified for nperseg is different" " from length of window"
+                )
+    return win, nperseg
+
+
+def estimate_spectrum(
+    xnt: np.array,
+    ynt=None,
+    fs=1.0,
+    window="hann",
+    nperseg=None,
+    noverlap=None,
+    nfft=None,
+    detrend="constant",
+    return_onesided=True,
+    scaling="spectrum",  # default scaling from scipy
+    axis=-1,
+    freq_minmax=[0, np.inf],
+    abs=False,
+    alltoall=True,  # every x with every y
+    return_coefs=False,
+    y_in_coefs=False,
+    x_in_coefs=False,
+    normalize_per_trial=False,
+):
+    """Calculate various forms of windowed FFTs for PSD, CSD, etc.
+
+    This is a helper function that implements the commonality between
+    the stft, psd, csd, and spectrogram functions. It is not designed to
+    be called externally. The windows are not averaged over; the result
+    from each window is returned.
+
+    Parameters
+    ----------
+    x : array_like
+        Array or sequence containing the data to be analyzed.
+    y : array_like
+        Array or sequence containing the data to be analyzed. If this is
+        the same object in memory as `x` (i.e. ``_spectral_helper(x,
+        x, ...)``), the extra computations are spared.
+    fs : float, optional
+        Sampling frequency of the time series. Defaults to 1.0.
+    window : str or tuple or array_like, optional
+        Desired window to use. If `window` is a string or tuple, it is
+        passed to `get_window` to generate the window values, which are
+        DFT-even by default. See `get_window` for a list of windows and
+        required parameters. If `window` is array_like it will be used
+        directly as the window and its length must be nperseg. Defaults
+        to a Hann window.
+    nperseg : int, optional
+        Length of each segment. Defaults to None, but if window is str or
+        tuple, is set to 256, and if window is array_like, is set to the
+        length of the window.
+    noverlap : int, optional
+        Number of points to overlap between segments. If `None`,
+        ``noverlap = nperseg // 2``. Defaults to `None`.
+    nfft : int, optional
+        Length of the FFT used, if a zero padded FFT is desired. If
+        `None`, the FFT length is `nperseg`. Defaults to `None`.
+    detrend : str or function or `False`, optional
+        Specifies how to detrend each segment. If `detrend` is a
+        string, it is passed as the `type` argument to the `detrend`
+        function. If it is a function, it takes a segment and returns a
+        detrended segment. If `detrend` is `False`, no detrending is
+        done. Defaults to 'constant'.
+    return_onesided : bool, optional
+        If `True`, return a one-sided spectrum for real data. If
+        `False` return a two-sided spectrum. Defaults to `True`, but for
+        complex data, a two-sided spectrum is always returned.
+    scaling : { 'density', 'spectrum' }, optional
+        Selects between computing the cross spectral density ('density')
+        where `Pxy` has units of V**2/Hz and computing the cross
+        spectrum ('spectrum') where `Pxy` has units of V**2, if `x`
+        and `y` are measured in V and `fs` is measured in Hz.
+        Defaults to 'density'
+    axis : int, optional
+        Axis along which the FFTs are computed; the default is over the
+        last axis (i.e. ``axis=-1``).
+
+    Returns
+    -------
+    freqs : ndarray
+        Array of sample frequencies.
+    t : ndarray
+        Array of times corresponding to each data segment
+    result : ndarray
+        Array of output data, contents dependent on *mode* kwarg.
+
+    """
+
+    if not x_in_coefs:
+        if len(xnt.shape) < 2:
+            xnt = xnt[None]
+        xn = xnt.shape[0]
+        coefs_xnkf, freqs = compute_spectral_coefs(
+            xnt=xnt,
+            fs=fs,
+            window=window,
+            nperseg=nperseg,
+            noverlap=noverlap,
+            detrend=detrend,
+            return_onesided=return_onesided,
+            scaling=scaling,
+            axis=axis,
+            freq_minmax=freq_minmax,
+            nfft=nfft,
+        )
+
+    else:
+        xn = xnt.shape[0]
+        coefs_xnkf = xnt  # assume xnt is already N x K x F
+        f = coefs_xnkf.shape[-1]
+        freqs = sp_fft.rfftfreq(f * 2 - 1, d=1 / fs)  # to check
+
+    if ynt is not None:
+        if not y_in_coefs:
+            if len(ynt.shape) < 2:
+                ynt = ynt[None]
+            yn = ynt.shape[0]
+            coefs_ynkf, _ = compute_spectral_coefs(
+                xnt=ynt,
+                fs=fs,
+                window=window,
+                nperseg=nperseg,
+                noverlap=noverlap,
+                detrend=detrend,
+                return_onesided=return_onesided,
+                scaling=scaling,
+                axis=axis,
+                freq_minmax=freq_minmax,
+                nfft=nfft,
+            )
+
+        else:
+            yn = ynt.shape[0]
+            coefs_ynkf = ynt
+        if not alltoall:
+            if yn != xn:
+                raise Exception("invalid dimensions for not all to all")
+
+    wn = coefs_xnkf.shape[-1]
+    kn = coefs_xnkf.shape[1]
+    coefs_xnkf = da.from_array(coefs_xnkf, chunks=("auto"))
+    if ynt is not None:
+        coefs_ynkf = da.from_array(coefs_ynkf, chunks=("auto"))
+        if alltoall:
+            # pxy = jnp.einsum("nkf, mkf-> nmf", coefs_xnkf, np.conj(coefs_ynkf))
+            pxy = da.einsum("nkf, mkf-> nmf", coefs_xnkf, np.conj(coefs_ynkf), optimize = True)
+        else:
+            # pxy = jnp.einsum("nkf, nkf-> nf", coefs_xnkf, np.conj(coefs_ynkf))
+            pxy = da.einsum("nkf, nkf-> nf", coefs_xnkf, np.conj(coefs_ynkf), optimize = True)
+
+    else:
+        if alltoall:
+            # pxy = jnp.einsum("nkf, mkf-> nmf", coefs_xnkf, np.conj(coefs_xnkf))
+            pxy = da.einsum("nkf, mkf-> nmf", coefs_xnkf, da.conj(coefs_xnkf), optimize = True)
+        else:
+            # pxy = jnp.einsum("nkf, nkf-> nf", coefs_xnkf, np.conj(coefs_xnkf))
+            pxy = da.einsum("nkf, nkf-> nf", coefs_xnkf, da.conj(coefs_xnkf), optimize = True)
+
+    if abs:
+        pxy = np.abs(pxy).real
+    if normalize_per_trial:
+        pxy /= kn
+    if return_coefs:
+        if ynt is not None:
+            return pxy, freqs, coefs_xnkf, coefs_ynkf
+        else:
+            return pxy, freqs, coefs_xnkf
+    else:
+        return pxy, freqs
+
+
+def get_freqs(nperseg: int, fs: float, freq_minmax=[0, np.inf]):
+    freqs = sp_fft.rfftfreq(nperseg, d=1 / fs)  # to check
+    valid_freq_inds = (np.abs(freqs) >= freq_minmax[0]) & (
+        np.abs(freqs) <= freq_minmax[1]
+    )
+    valid_freqs = freqs[valid_freq_inds]
+    return freqs, valid_freqs, valid_freq_inds
+
+
+def compute_multiscale_spectral_coefs(xnt: np.ndarray, fs: float, window: str, noverlap: int, detrend: str, return_onesided: bool, scaling: str, axis: int, num_levels: int = 10, reps: int = 3):
+    """Compute Spectral Fourier Coefs for a multiscale analysis"""
+
+    def detrend_func(d):
+        from scipy.signal import signaltools
+        return signaltools.detrend(d, type=detrend, axis=-1)
+
+    n, t = xnt.shape
+    N = t
+    freqs = get_freqs(t, fs)
+    lowest_freq = 1/(N/fs)
+    highest_freq = (N/2)/(N/fs) # i.e.: 2/fps
+    freqs_ = (np.arange(1, int(N/2))/(N/fs))[::(int(N/2)//num_levels)]
+
+
+
+    spectra = []
+    freqs_all = []
+    for ind, freq in enumerate(freqs_):
+        frame_by_cycle = (1/freq)*fs
+        nperseg = int(frame_by_cycle)*reps
+        # print(f"freq: {freq}, nperseg: {nperseg}")
+        win, nperseg = _triage_segments(window, nperseg, input_length=t) 
+        nfft = nperseg # win is a 1d array
+        noverlap = nperseg // 2
+
+        if noverlap >= nperseg:
+            print(f"noverlap: {noverlap}, nperseg: {nperseg}")
+            raise ValueError("noverlap must be less than nperseg.")
+
+        if return_onesided:
+            if np.iscomplexobj(xnt):
+                sides = "twosided"
+            else:
+                sides = "onesided"
+        else:
+            sides = "twosided"
+
+        if sides == "twosided":
+            freqs = sp_fft.fftfreq(nfft, 1 / fs)
+        elif sides == "onesided":
+            freqs = sp_fft.rfftfreq(nfft, 1 / fs)
+
+        if scaling == "density":
+            scale = 1.0 / (fs * (win * win).sum())
+        elif scaling == "spectrum":
+            scale = 1.0 / win.sum() ** 2
+        else:
+            raise ValueError("Unknown scaling: %r" % scaling)
+
+        if detrend is False:
+            detrend_func = None
+        coefs_xnkf = myfft_helper(xnt, win, detrend_func, nperseg, noverlap, nfft, sides)
+        coefs_xnkf *= scale
+        spectra.append(coefs_xnkf)
+        freqs_all.append(freqs)
+
+    return spectra, freqs_all
+
+
+  
+def wrangle_multiscale_coefs(coefs_xnkfs, freqs_):
+    from scipy.interpolate import interp1d
+    min_freq = 100
+    final_freqs = []
+    final_coefs_xnkfs = []
+    for ind, freqs in enumerate(freqs_[::-1]):
+        freqs = freqs[1:]
+        vinds = np.argwhere(freqs<min_freq)[:,0]
+        if len(vinds) > 0:
+            freq_v = freqs[vinds]
+            min_freq = np.min(freq_v)
+            coefs_xnkf = coefs_xnkfs[::-1][ind][0, :, 1:][:, vinds]
+            final_freqs.append(freq_v)
+            final_coefs_xnkfs.append(np.abs(coefs_xnkf))
+    final_freqs = np.hstack(final_freqs[::-1])
+    final_coefs_xnkfs = final_coefs_xnkfs[::-1]
+    
+    max_len = final_coefs_xnkfs[-1].shape[0]
+    coefs_interp = []
+    for ind, coefs_xnkf in enumerate(final_coefs_xnkfs):
+        coefs_xnkf_interp = np.zeros((max_len, coefs_xnkf.shape[1]))
+        # interpolate the coefs_xnkf to the max_len using closest neighbor
+        for i in range(coefs_xnkf.shape[1]):
+            # Use scipy.interpolate.interp1d for nearest neighbor interpolation
+            f_interp = interp1d(np.linspace(0, 1, coefs_xnkf.shape[0]), coefs_xnkf[:, i], kind="nearest", bounds_error=False, fill_value="extrapolate")
+            coefs_xnkf_interp[:, i] = f_interp(np.linspace(0, 1, max_len))
+            
+        coefs_interp.append(coefs_xnkf_interp)
+    coefs_interp = np.hstack(coefs_interp)
+
+
+    min_freq = final_freqs[0]
+    max_freq = final_freqs[-1]
+    spectrogram = coefs_interp.T[::-1]
+    return spectrogram, final_freqs, min_freq, max_freq  
+
+def compute_spectral_coefs(  # used in coherence
+    xnt: np.ndarray,
+    fs=1.0,
+    window="hann",
+    nperseg=None,
+    noverlap=None,
+    detrend="constant",
+    return_onesided=True,
+    scaling="spectrum",
+    axis=-1,
+    freq_minmax=[0, np.inf],
+    nfft=None,
+):
+    """Compute Spectral Fourier Coefs"""
+
+    def detrend_func(d):
+        from scipy.signal import signaltools
+
+        return signaltools.detrend(d, type=detrend, axis=-1)
+
+    n, t = xnt.shape
+    win, nperseg = _triage_segments(
+        window, nperseg, input_length=t
+    )  # win is a 1d array
+
+    if nfft is None:
+        nfft = nperseg
+    elif nfft < nperseg:
+        raise ValueError("nfft must be greater than or equal to nperseg.")
+    else:
+        nfft = int(nfft)
+
+    if noverlap is None:
+        noverlap = nperseg // 2
+    else:
+        noverlap = int(noverlap)
+    if noverlap >= nperseg:
+        raise ValueError("noverlap must be less than nperseg.")
+
+    if return_onesided:
+        if np.iscomplexobj(xnt):
+            sides = "twosided"
+        else:
+            sides = "onesided"
+    else:
+        sides = "twosided"
+
+    if sides == "twosided":
+        freqs = sp_fft.fftfreq(nfft, 1 / fs)
+    elif sides == "onesided":
+        freqs = sp_fft.rfftfreq(nfft, 1 / fs)
+
+    if scaling == "density":
+        scale = 1.0 / (fs * (win * win).sum())
+    elif scaling == "spectrum":
+        scale = 1.0 / win.sum() ** 2
+    else:
+        raise ValueError("Unknown scaling: %r" % scaling)
+
+    if detrend is False:
+        detrend_func = None
+    coefs_xnkf = myfft_helper(xnt, win, detrend_func, nperseg, noverlap, nfft, sides)
+    coefs_xnkf *= scale
+
+    valid_freqs = (np.abs(freqs) >= freq_minmax[0]) & (np.abs(freqs) <= freq_minmax[1])
+    freqs = freqs[valid_freqs]
+    coefs_xnkf = coefs_xnkf[:, :, valid_freqs]
+
+    return coefs_xnkf, freqs
+
+
+def myfft_helper(
+    x: np.ndarray,
+    win: np.ndarray,
+    detrend_func,
+    nperseg: int,
+    noverlap: int,
+    nfft,
+    sides: str,
+):
+    """
+    Calculate windowed FFT, for internal use by
+    `scipy.signal._spectral_helper`.
+    This is a helper function that does the main FFT calculation for
+    `_spectral helper`. All input validation is performed there, and the
+    data axis is assumed to be the last axis of x. It is not designed to
+    be called externally. The windows are not averaged over; the result
+    from each window is returned.
+    Returns
+    -------
+    result : ndarray
+        Array of FFT data
+    Notes
+    -----
+    Adapted from matplotlib.mlab
+    .. versionadded:: 0.16.0
+    """
+    # Created strided array of data segments
+    if nperseg == 1 and noverlap == 0:
+        result = x[..., np.newaxis]
+    else:
+        result = make_chunks(x, nperseg, noverlap)
+
+    # Detrend each data segment individually
+    if detrend_func is not None:
+        result = detrend_func(result)  # default to last axis - result is N x K x T
+
+    # Apply window by multiplication
+
+    result = win * result
+
+    # Perform the fft. Acts on last axis by default. Zero-pads automatically
+    if sides == "twosided":
+        func = sp_fft.fft
+    else:
+        result = (
+            result.real
+        )  # forces result to be real (should be real anyway as doing one sided fft...)
+        func = sp_fft.rfft
+    coefs_nkf = func(result, n=nfft)
+
+    return coefs_nkf
+
+
+
+
---
+++ src/ParticleGraph/spectral_utils/spectral_test.py
@@ -0,0 +1,188 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+np.random.seed(42)
+import myspectral_funcs
+from importlib import reload
+
+
+save_path = "./figs/"
+
+# --------------------------
+# 1. Set up the time grid
+# --------------------------
+T = 50.0            # Total time duration
+fps = 10
+N = int(T*fps)            # Number of time points
+t = np.linspace(0, T, N)  # Time vector from 0 to T
+
+# --------------------------
+# 2. Define main GP kernels
+# --------------------------
+# Low- and high-frequency oscillation parameters
+f_low = 0.5         # Low-frequency (Hz)
+f_high = 5.0        # High-frequency (Hz)
+ell_low = 1.0       # Lengthscale for low-frequency kernel
+ell_high = 0.2      # Lengthscale for high-frequency kernel
+sigma_low = 1.0     # Amplitude for low-frequency kernel
+sigma_high = 0.5    # Amplitude for high-frequency kernel
+
+def oscillatory_kernel(t1, t2, f, ell, sigma):
+    """
+    Oscillatory Gaussian kernel:
+      k(t1, t2) = sigma^2 * exp(-(t1-t2)^2/(2*ell^2)) * cos(2*pi*f*(t1-t2))
+    Combines smooth Gaussian decay with cosine oscillation at frequency f.
+    """
+    dt = t1 - t2
+    return sigma**2 * np.exp(-0.5 * (dt/ell)**2) * np.cos(2 * np.pi * f * dt)
+
+# Precompute stationary covariance matrices for low and high components
+K_low  = np.array([[oscillatory_kernel(t[i], t[j], f_low,  ell_low,  sigma_low)  for j in range(N)] for i in range(N)])
+K_high = np.array([[oscillatory_kernel(t[i], t[j], f_high, ell_high, sigma_high) for j in range(N)] for i in range(N)])
+
+# --------------------------
+# 3. Build envelope processes
+# --------------------------
+# Very-low-frequency GP parameters for envelopes
+f_env    = 0.001      # Envelope oscillation frequency (Hz)
+ell_env  = 3.0      # Envelope lengthscale
+sigma_env= 1.0      # Envelope variance
+
+# Build covariance for the envelope GP
+K_env = np.zeros((N, N))
+for i in range(N):
+    for j in range(N):
+        K_env[i, j] = oscillatory_kernel(t[i], t[j], f_env, ell_env, sigma_env)
+K_env += 1e-6 * np.eye(N)  # Add small nugget for numerical stability
+
+# Sample two independent envelopes for low- and high-frequency bands
+e_low  = np.linspace(-10, 10, N)
+e_high  = np.random.multivariate_normal(np.zeros(N), K_env)
+envelope_f_low = 0.01
+envelope_f_high = 0.03
+# e_low = np.sin(2*np.pi*envelope_f_low*t)
+e_high = np.sin(2*np.pi*envelope_f_high*t)*10
+# e_high = np.ones(N)
+# e_high = np.random.multivariate_normal(np.zeros(N), K_env)
+
+# Convert raw GP draws to (0,1) via a sigmoid so they can serve as weights
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+w_low  = sigmoid(e_low)   # Time-varying weight for the low-frequency kernel
+w_high = sigmoid(e_high)  # Time-varying weight for the high-frequency kernel
+
+
+# --------------------------
+# 4. Build nonstationary covariance
+# --------------------------
+K_ns = np.zeros((N, N))
+for i in range(N):
+    for j in range(N):
+        # Each kernel is modulated by the product of its envelope values
+        K_ns[i, j] = w_low[i]*w_low[j]*K_low[i, j] + w_high[i]*w_high[j]*K_high[i, j]
+K_ns += 1e-6 * np.eye(N)  # Final nugget for stability
+
+# --------------------------
+# 5. Sample from the nonstationary GP
+# --------------------------
+y_ns = np.random.multivariate_normal(np.zeros(N), K_ns)
+
+#%%
+
+reload(myspectral_funcs)
+xnt = y_ns[None]
+ynt = None
+fs = fps
+window = "hann"
+nperseg = int(fs*4)
+noverlap = None
+nfft = None
+detrend = "constant"
+return_onesided = True
+scaling = "spectrum"
+abs = True
+return_coefs = True
+
+pxy, freqs, coefs_xnkf = myspectral_funcs.estimate_spectrum(xnt, ynt =ynt, fs=fs, window=window, nperseg=nperseg, noverlap=noverlap, nfft=nfft, detrend=detrend, return_onesided=return_onesided, scaling=scaling, abs=abs, return_coefs=return_coefs)
+coefs_xnkf = coefs_xnkf.compute()
+pxy = pxy.compute()
+
+#%%
+# --------------------------
+# 6. Plot envelopes and GP realization
+# --------------------------
+fig, ax = plt.subplots(2, 1, figsize=(8, 6), sharex=True)
+
+# Plot envelope processes
+ax[0].plot(t, w_low,  label='Low-Freq Envelope')
+ax[0].plot(t, w_high, label='High-Freq Envelope')
+ax[0].set_ylabel('Envelope Weight')
+ax[0].legend(loc='upper right')
+
+# Plot one sample from the nonstationary GP
+ax[1].plot(t, y_ns, color='tab:orange')
+ax[1].set_xlabel('Time')
+ax[1].set_ylabel('GP Value')
+ax[1].set_title('Nonstationary GP Sample with Random Envelopes')
+
+plt.tight_layout()
+plt.savefig(save_path + "gp_realization.png")
+
+plt.figure()
+plt.title("PSD")
+plt.plot(freqs, pxy[0,0])
+plt.savefig(save_path + "psd.png")
+
+min_freq = freqs[0]
+max_freq = freqs[-1]
+spectrogram = np.abs(coefs_xnkf[0]).T[::-1]
+xax = np.linspace(0, T, spectrogram.shape[1])
+plt.figure()
+cmap = plt.get_cmap("coolwarm")
+plt.matshow(spectrogram, extent = [xax[0], xax[-1], min_freq, max_freq], cmap=cmap, aspect="auto")
+plt.xlabel("Time (s)")
+plt.ylabel("Frequency (Hz)")
+plt.colorbar()
+plt.savefig(save_path + "spectrogram.png")
+#%%
+reload(myspectral_funcs)
+num_levels = 10
+reps = 5
+reload(myspectral_funcs)
+coefs_xnkfs, freqs_ = myspectral_funcs.compute_multiscale_spectral_coefs(xnt=y_ns[None], fs=fps, window="hann", noverlap=None, detrend="constant", return_onesided=True, scaling="spectrum", axis=0, num_levels = num_levels, reps = reps)
+
+spectrogram, final_freqs, min_freq, max_freq = myspectral_funcs.wrangle_multiscale_coefs(coefs_xnkfs, freqs_)
+
+xax = np.linspace(0, T, spectrogram.shape[1])
+plt.figure(figsize=(4, 3))
+cmap = plt.get_cmap("coolwarm")
+dxx = spectrogram.shape[1]
+dxy = spectrogram.shape[0]
+plt.matshow(spectrogram, extent = [xax[0], xax[-1], min_freq, max_freq], cmap=cmap, aspect=dxx/dxy)
+ds = 3
+plt.yticks(np.linspace(0, final_freqs[-1], len(final_freqs))[::ds], np.round(final_freqs[::ds], 2))
+# plt.xticks(np.arange(len(xax)), xax)
+plt.colorbar()
+plt.title("Multiscale Spectrogram")
+plt.xlabel("Time (s)")
+plt.ylabel("Frequency (Hz)")
+plt.savefig(save_path + "multiscale_spectrogram.png")
+
+
+
+
+
+#%%
+
+#%%
+# reload(myspectral_funcs)
+# N = len(t)
+# freqs = myspectral_funcs.get_freqs(nperseg=N, fs=fps)[0]
+
+# # lower frequency one can infer is a cycle of the full length of the signal
+# lowest_freq = 1/(N/fps)
+# highest_freq = (N/2)/(N/fps) # i.e.: 2/fps
+# num_levels = 10
+# freqs_ = (np.arange(0, int(N/2))/(N/fps))[::(int(N/2)//num_levels)]
+# freqs_ 
