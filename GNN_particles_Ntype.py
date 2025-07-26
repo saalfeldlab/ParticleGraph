0568a92f2543b36f41e150258e3bf9edfe29e9b0
allierc <allierc@janelia.hhmi.org>
2025-07-26 09:30:27 -0400

    add MPM_step to generator models

--- GNN_particles_Ntype.py
+++ GNN_particles_Ntype.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     else:
         task = 'generate'  # 'generate', 'train', 'test'
         best_model = ''
-        # config_list = ['multimaterial_1_1', 'multimaterial_1_2', 'multimaterial_1_3', 'multimaterial_1_4']
+        # config_list = ['multimaterial_1_2', 'multimaterial_1_3', 'multimaterial_1_4']
         config_list = ['multimaterial_1_0']
         # config_list = ['fluids_m19']
         # config_list = ['falling_water_ramp_x6_11_1']
--- src/ParticleGraph/generators/__init__.py
+++ src/ParticleGraph/generators/__init__.py
@@ -17,6 +17,7 @@ from .PDE_S import PDE_S
 from .PDE_V import PDE_V
 from .PDE_Z import PDE_Z
 from .MPM_P2G import *
+from .MPM_step import *
 from .RD_FitzHugh_Nagumo import RD_FitzHugh_Nagumo
 from .RD_Gray_Scott import RD_Gray_Scott
 from .RD_RPS import RD_RPS
@@ -25,5 +26,5 @@ from .utils import choose_model, choose_mesh_model, init_particles, init_mesh
 from .cell_utils import *
 
 __all__ = [utils, cell_utils, graph_data_generator, PDE_Laplacian, PDE_A, PDE_B, PDE_E, PDE_F, PDE_G, PDE_K,
-           PDE_N, PDE_N2, PDE_N3, PDE_N4, PDE_N5, PDE_N6, PDE_N7, PDE_O, PDE_V, PDE_Z, PDE_S, MPM_P2G,
+           PDE_N, PDE_N2, PDE_N3, PDE_N4, PDE_N5, PDE_N6, PDE_N7, PDE_O, PDE_V, PDE_Z, PDE_S, MPM_P2G, MPM_step,
            RD_FitzHugh_Nagumo, RD_Gray_Scott, RD_RPS, choose_model, choose_mesh_model, init_particles, init_mesh]
