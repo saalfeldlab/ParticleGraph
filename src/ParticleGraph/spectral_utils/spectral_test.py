0d78c3aa2305869634b87621cbde440fe252d75b
vruetten <vms.ruetten@gmail.com>
2025-07-30 10:33:12 -0400

    add saving of 3 variable spectral demo data

--- src/ParticleGraph/spectral_utils/spectral_test.py
+++ src/ParticleGraph/spectral_utils/spectral_test.py
@@ -1,19 +1,23 @@
 #%%
 import matplotlib
-matplotlib.use('Agg')  # Use non-interactive backend for headless environments
+# matplotlib.use('Agg')  # Use non-interactive backend for headless environments
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
 np.random.seed(42)
 import myspectral_funcs
 from importlib import reload
-
+import os
 
 save_path = "./figs/"
+data_path = "./data/"
+os.makedirs(data_path, exist_ok=True)
+os.makedirs(save_path, exist_ok=True)
+
 
 # --------------------------
 # 1. Set up the time grid
 # --------------------------
-T = 50.0            # Total time duration
+T = 300.0            # Total time duration
 fps = 10
 N = int(T*fps)            # Number of time points
 t = np.linspace(0, T, N)  # Time vector from 0 to T
@@ -22,13 +26,12 @@ t = np.linspace(0, T, N)  # Time vector from 0 to T
 # 2. Define main GP kernels
 # --------------------------
 # Low- and high-frequency oscillation parameters
-f_low = 0.5         # Low-frequency (Hz)
-f_high = 5.0        # High-frequency (Hz)
-ell_low = 1.0       # Lengthscale for low-frequency kernel
-ell_high = 0.2      # Lengthscale for high-frequency kernel
+f_low = 0.1         # Low-frequency (Hz)
+f_high = 2.0        # High-frequency (Hz)
+ell_low = 2.0       # Lengthscale for low-frequency kernel
+ell_high = 0.9      # Lengthscale for high-frequency kernel
 sigma_low = 1.0     # Amplitude for low-frequency kernel
 sigma_high = 0.5    # Amplitude for high-frequency kernel
-
 def oscillatory_kernel(t1, t2, f, ell, sigma):
     """
     Oscillatory Gaussian kernel:
@@ -42,6 +45,12 @@ def oscillatory_kernel(t1, t2, f, ell, sigma):
 K_low  = np.array([[oscillatory_kernel(t[i], t[j], f_low,  ell_low,  sigma_low)  for j in range(N)] for i in range(N)])
 K_high = np.array([[oscillatory_kernel(t[i], t[j], f_high, ell_high, sigma_high) for j in range(N)] for i in range(N)])
 
+# fig, ax = pl.subplots(1, 2, figsize=(8, 4))
+# ax[0].imshow(K_low)
+# ax[1].imshow(K_high)
+
+
+#%%
 # --------------------------
 # 3. Build envelope processes
 # --------------------------
@@ -89,11 +98,23 @@ K_ns += 1e-6 * np.eye(N)  # Final nugget for stability
 # 5. Sample from the nonstationary GP
 # --------------------------
 y_ns = np.random.multivariate_normal(np.zeros(N), K_ns)
-
 #%%
 
 reload(myspectral_funcs)
-xnt = y_ns[None]
+xnt = np.array([y_ns, w_low, w_high])
+
+pl.figure()
+pl.plot(t, y_ns)
+pl.plot(t, w_low)
+pl.plot(t, w_high)
+# pl.savefig(save_path + "gp_realization.png")
+# pl.close()
+
+
+
+#%%
+
+# xnt = y_ns[None]
 ynt = None
 fs = fps
 window = "hann"
@@ -111,10 +132,19 @@ coefs_xnkf = coefs_xnkf.compute()
 pxy = pxy.compute()
 
 #%%
+results = {}
+results["xnt"] = xnt
+results["fs"] = fs
+results["coefs_xnkf"] = np.abs(coefs_xnkf[0]).real
+results["freqs"] = freqs
+np.save(data_path + "results.npy", results)
+
+
+#%%
 # --------------------------
 # 6. Plot envelopes and GP realization
 # --------------------------
-fig, ax = plt.subplots(2, 1, figsize=(8, 6), sharex=True)
+fig, ax = pl.subplots(2, 1, figsize=(8, 6), sharex=True)
 
 # Plot envelope processes
 ax[0].plot(t, w_low,  label='Low-Freq Envelope')
@@ -128,28 +158,28 @@ ax[1].set_xlabel('Time')
 ax[1].set_ylabel('GP Value')
 ax[1].set_title('Nonstationary GP Sample with Random Envelopes')
 
-plt.tight_layout()
-plt.savefig(save_path + "gp_realization.png")
-plt.close()
+pl.tight_layout()
+pl.savefig(save_path + "gp_realization.png")
+pl.close()
 
-plt.figure()
-plt.title("PSD")
-plt.plot(freqs, pxy[0,0])
-plt.savefig(save_path + "psd.png")
-plt.close()
+pl.figure()
+pl.title("PSD")
+pl.plot(freqs, pxy[0,0])
+pl.savefig(save_path + "psd.png")
+pl.close()
 
 min_freq = freqs[0]
 max_freq = freqs[-1]
 spectrogram = np.abs(coefs_xnkf[0]).T[::-1]
 xax = np.linspace(0, T, spectrogram.shape[1])
-plt.figure()
-cmap = plt.get_cmap("coolwarm")
-plt.matshow(spectrogram, extent = [xax[0], xax[-1], min_freq, max_freq], cmap=cmap, aspect="auto")
-plt.xlabel("Time (s)")
-plt.ylabel("Frequency (Hz)")
-plt.colorbar()
-plt.savefig(save_path + "spectrogram.png")
-plt.close()
+pl.figure()
+cmap = pl.get_cmap("coolwarm")
+pl.matshow(spectrogram, extent = [xax[0], xax[-1], min_freq, max_freq], cmap=cmap, aspect="auto")
+pl.xlabel("Time (s)")
+pl.ylabel("Frequency (Hz)")
+pl.colorbar()
+pl.savefig(save_path + "spectrogram.png")
+
 
 #%%
 reload(myspectral_funcs)
@@ -162,19 +192,19 @@ spectrogram, final_freqs, min_freq, max_freq = myspectral_funcs.wrangle_multisca
 
 xax = np.linspace(0, T, spectrogram.shape[1])
 
-plt.figure(figsize=(4, 3))
-cmap = plt.get_cmap("coolwarm")
+pl.figure(figsize=(4, 3))
+cmap = pl.get_cmap("coolwarm")
 dxx = spectrogram.shape[1]
 dxy = spectrogram.shape[0]
-plt.matshow(spectrogram, extent = [xax[0], xax[-1], min_freq, max_freq], cmap=cmap, aspect=dxx/dxy)
+pl.matshow(spectrogram, extent = [xax[0], xax[-1], min_freq, max_freq], cmap=cmap, aspect=dxx/dxy)
 ds = 3
-plt.yticks(np.linspace(0, final_freqs[-1], len(final_freqs))[::ds], np.round(final_freqs[::ds], 2))
+pl.yticks(np.linspace(0, final_freqs[-1], len(final_freqs))[::ds], np.round(final_freqs[::ds], 2))
 # plt.xticks(np.arange(len(xax)), xax)
-plt.colorbar()
-plt.title("Multiscale Spectrogram")
-plt.xlabel("Time (s)")
-plt.ylabel("Frequency (Hz)")
-plt.savefig(save_path + "multiscale_spectrogram.png")
+pl.colorbar()
+pl.title("Multiscale Spectrogram")
+pl.xlabel("Time (s)")
+pl.ylabel("Frequency (Hz)")
+pl.savefig(save_path + "multiscale_spectrogram.png")
 plt.close()
 
 
